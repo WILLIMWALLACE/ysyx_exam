5268398778957965f82a19dec30388b4d032bb5c
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 21:05:04 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:04 up 1 day, 10:38,  1 user,  load average: 0.25, 0.25, 0.17

---
+++ npc/csrc/top_tb.cpp
@@ -0,0 +1,46 @@
+   #include <stdio.h>
+   #include <stdlib.h>
+   #include <assert.h>
+   #include <Vtop.h>
+   #include <verilated.h>
+   #include <memory>
+   #include <verilated_vcd_c.h>
+   
+   vluint64_t   main_time = 0; //仿真时间
+  
+  double sc_time_stamp() { return  main_time  ; }
+  
+  int main(int argc, char** argv, char** env) {
+   
+     Verilated::commandArgs(argc, argv);          // Remember args
+     Verilated::traceEverOn(true);                // 导出波形必备  命令行里还需--trace
+     VerilatedVcdC*  tfp = new  VerilatedVcdC;    //导出波形需用，且可根据tfp设置trace追踪的层次
+     Vtop *top = new Vtop("top");
+     
+     top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
+     // tfp->dumpvars(1, "t");  // trace 1 level under "t"
+     tfp->open("test.vcd");
+  
+    while (sc_time_stamp() < 20 && !Verilated::gotFinish()) {                                                                                                                                            
+  
+          int a = rand() & 1;
+          int b = rand() & 1;
+          top->a = a;
+          top->b = b;                                             //top指针操作，赋值a，b
+          top->eval();                                            //执行一次erilog代码 更新值
+          tfp->dump(main_time);
+          main_time++;                                            //更新仿真时间
+          
+          printf("a = %d, b = %d, f = %d\n", a, b, top->f);       //看看而已
+          assert(top->f == a ^ b);                                //断言检测 
+          
+  
+     }
+          top->final();
+          tfp->close();
+          delete top;
+          
+          return 0;
+  }   
+  
+
