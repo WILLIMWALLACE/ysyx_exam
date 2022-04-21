af16a6c4afdd68a94b32ab0e0e0c83ee0a806f49
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 19:39:35 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:39:35 up 3 days,  9:13,  1 user,  load average: 0.09, 0.34, 0.41

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -14,12 +14,12 @@
    void nvboard_bind_all_pins(Vtop* top);
    Vtop *top = new Vtop("top");  
    
-   static void single_cycle() {
+   void single_cycle() {
   	top->clk = 0; top->eval();
   	top->clk = 1; top->eval();
    }
 
-   static void reset(int n) {
+   void reset(int n) {
   	top->rst = 1;
   	while (n -- > 0) single_cycle();
   	top->rst = 0;
