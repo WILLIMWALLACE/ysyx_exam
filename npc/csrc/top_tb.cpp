0d49fa83f3e001aba850ad45adcc6b651a7280f1
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 20:42:13 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:42:13 up 3 days, 10:15,  1 user,  load average: 0.15, 0.22, 0.21

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -37,7 +37,7 @@
      Vtop *top = new Vtop("top"); 		  //指向Vtop类的 top地址的指针，new分配内存空间并且调用Vtop的构造函数，新建了一个对象（类）top
      
      top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
-     tfp->dumpvars(1, "t");  // trace 1 level under "t"
+    // tfp->dumpvars(1, "t");  // trace 1 level under "t"
      tfp->open("test.vcd");
   /////////////////////////////产生波形固定内容
 
