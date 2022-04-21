0bb4a222646f93705964e292f4e20bdc3b8c4246
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 19:10:10 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:10 up 3 days,  8:43,  1 user,  load average: 0.23, 0.18, 0.18

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -12,9 +12,10 @@
 
    double sc_time_stamp() { return  main_time  ; }
    void nvboard_bind_all_pins(Vtop* top);
+   Vtop *top = new Vtop("top");  
    
    static void single_cycle() {
-	     Vtop *top = new Vtop("top");
+//	Vtop *top = new Vtop("top");
   	top->clk = 0; top->eval();
   	top->clk = 1; top->eval();
    }
@@ -34,7 +35,7 @@
      Verilated::commandArgs(argc, argv);          // Remember args
      Verilated::traceEverOn(true);                // 导出波形必备  命令行里还需--trace
      VerilatedVcdC*  tfp = new  VerilatedVcdC;    //导出波形需用，且可根据tfp设置trace追踪的层次
-     Vtop *top = new Vtop("top"); 		  //指向Vtop类的 top地址的指针，new分配内存空间并且调用Vtop的构造函数，新建了一个对象（类）top
+//     Vtop *top = new Vtop("top"); 		  //指向Vtop类的 top地址的指针，new分配内存空间并且调用Vtop的构造函数，新建了一个对象（类）top
      
      top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
      // tfp->dumpvars(1, "t");  // trace 1 level under "t"
