88ca466ef0fef302d2897fb329e0cb41c645d173
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 19:00:21 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:00:20 up 3 days,  8:33,  1 user,  load average: 0.40, 0.22, 0.19

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -10,14 +10,27 @@
    vluint64_t   main_time = 0; //仿真时间
    static TOP_NAME dut;
 
-  double sc_time_stamp() { return  main_time  ; }
-  void nvboard_bind_all_pins(Vtop* top);
+   double sc_time_stamp() { return  main_time  ; }
+   void nvboard_bind_all_pins(Vtop* top);
+   
+   void single_cycle() {
+  	top->clk = 0; top->eval();
+  	top->clk = 1; top->eval();
+   }
+
+   void reset(int n) {
+  	top->rst = 1;
+  	while (n -- > 0) single_cycle();
+  	top->rst = 0;
+   }
 
   int main(int argc, char** argv, char** env) {
+     //接入nvboard
      nvboard_bind_all_pins(&dut); 
      nvboard_init();
-
-    Verilated::commandArgs(argc, argv);          // Remember args
+     reset(10);
+     //固定内容//////////////////////////
+     Verilated::commandArgs(argc, argv);          // Remember args
      Verilated::traceEverOn(true);                // 导出波形必备  命令行里还需--trace
      VerilatedVcdC*  tfp = new  VerilatedVcdC;    //导出波形需用，且可根据tfp设置trace追踪的层次
      Vtop *top = new Vtop("top"); 		  //指向Vtop类的 top地址的指针，new分配内存空间并且调用Vtop的构造函数，新建了一个对象（类）top
@@ -25,24 +38,26 @@
      top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
      // tfp->dumpvars(1, "t");  // trace 1 level under "t"
      tfp->open("test.vcd");
-  
+  /////////////////////////////产生波形固定内容
+
     // while (sc_time_stamp() < 20 && !Verilated::gotFinish()) {                                                                                                                                            
   while (!Verilated::gotFinish()) {
   	  nvboard_update();
-
+	  single_cycle();
         //  int a = rand() & 1;
          // int b = rand() & 1;
          // top->a = a;
          // top->b = b;                                             //top指针操作，赋值a，b
-          top->eval();                                            //执行一次erilog代码 更新值
-          tfp->dump(main_time);
+         // top->eval();    //执行一次erilog代码 更新值
+         
+	  tfp->dump(main_time);
           main_time++;                                            //更新仿真时间
           
         //  printf("a = %d, b = %d, f = %d\n", a, b, top->f);       //看看而已
         //  assert(top->f == a ^ b);                                //断言检测 
           
   
-     }
+      }
    //  	  nvboard_quit();
           top->final();
           tfp->close();
