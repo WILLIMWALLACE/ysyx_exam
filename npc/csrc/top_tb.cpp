a2639050b154c491131da7d38bc68647358df129
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 18:46:49 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:46:49 up 3 days,  8:20,  1 user,  load average: 0.30, 0.23, 0.18

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -38,8 +38,8 @@
           tfp->dump(main_time);
           main_time++;                                            //更新仿真时间
           
-          printf("a = %d, b = %d, f = %d\n", a, b, top->f);       //看看而已
-          assert(top->f == a ^ b);                                //断言检测 
+        //  printf("a = %d, b = %d, f = %d\n", a, b, top->f);       //看看而已
+        //  assert(top->f == a ^ b);                                //断言检测 
           
   
      }
