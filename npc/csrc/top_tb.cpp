124af343ff6f285864de85637e6983b908463b7d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-20 22:44:43 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:44:43 up  2:44,  1 user,  load average: 0.12, 0.42, 0.36

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -52,11 +52,11 @@
           top->eval();    //执行一次erilog代码 更新值
         // int a = top->a;
 	// int b = top->b;
-	 int f = top->f;
+	// int f = top->f;
 	 tfp->dump(main_time);
     //      main_time++;                                            //更新仿真时间
           
-          printf("sw0 = %d, sw1 = %d, ld0 = %d\n", a, b, f);       //看看而已
+    //      printf("sw0 = %d, sw1 = %d, ld0 = %d\n", a, b, f);       //看看而已
           assert(top->f == a ^ b);                                //断言检测 
           
   
