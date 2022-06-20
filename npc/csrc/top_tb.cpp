50c768ba2de648322a1b135f0b9e27fe76727b90
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-20 22:44:01 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:44:01 up  2:43,  1 user,  load average: 0.26, 0.49, 0.38

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -45,10 +45,10 @@
   while (1) {
   	  nvboard_update();
 	 // single_cycle();
-          int a = rand() & 1;
-          int b = rand() & 1;
-          top->a = a;
-          top->b = b;      //top指针操作，赋值a，b
+         // int a = rand() & 1;
+         // int b = rand() & 1;
+         //  top->a = a;
+         //  top->b = b;      //top指针操作，赋值a，b
           top->eval();    //执行一次erilog代码 更新值
         // int a = top->a;
 	// int b = top->b;
