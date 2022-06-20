eae0d22ff1336cbe6380929fd674d01cccc2704e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-20 23:44:19 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:44:19 up  3:44,  1 user,  load average: 0.35, 0.18, 0.25

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,29 +1,18 @@
 module top(
-  input  [7:0]  sw,
-  input  clk,
-  input  rst,
-  output [15:0] ledr
+  input clk,
+  input rst,
+  output reg [15:0] ledr
 );
- 
-     reg	f;
-     reg [31:0] count;
-     reg [7:0] led;
-    
-     assign f =  sw[0] ^ sw[1];
-
-     always @(posedge clk) begin
-      if (rst) begin led <= 1; count <= 0; end
-      else begin
-        if (count == 0) led <= {led[6:0], led[7]};
-        count <= (count >= 5000000 ? 32'b0 : count + 1);
-      end
-    end
+  reg [31:0] count;
   
-    assign ledr = {led, sw[7:2], 1'b1, f};
-
-
-
-
+  always @(posedge clk) begin
+    if (rst) begin led <= 1; count <= 0; end
+    else begin
+      if (count == 0) led <= {led[14:0], led[15]};
+      count <= (count >= 5000000 ? 32'b0 : count + 1);
+    end
+  end
+  assign  ledr = {led[15:0]};
 
 endmodule
 
