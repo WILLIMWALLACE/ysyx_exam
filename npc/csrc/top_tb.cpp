2d88cc1ea16ae096f9b4a347a5230027196a623f
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 21:43:38 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:38 up 1 day,  1:43,  1 user,  load average: 0.15, 0.10, 0.09

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -7,8 +7,9 @@
    #include <memory>
    #include <verilated_vcd_c.h>
    
-  // vluint64_t   main_time = 0; //仿真时间
-  // double sc_time_stamp() { return  main_time  ; }     
+   vluint64_t   main_time = 0; //仿真时间
+   double sc_time_stamp() { return  main_time  ; }     
+
    static TOP_NAME dut;
    void nvboard_bind_all_pins(Vtop* top);
   // Vtop *top = new Vtop("top");  
@@ -29,38 +30,44 @@
      nvboard_bind_all_pins(&dut); 
      nvboard_init();
      reset(10);
-     //固定内容//////////////////////////
+
+     ///////////////////////////固定内容   verilator init/////////////////////////////
      Verilated::commandArgs(argc, argv);          // Remember args
      Verilated::traceEverOn(true);                // 导出波形必备  命令行里还需--trace
      VerilatedVcdC*  tfp = new  VerilatedVcdC;    //导出波形需用，且可根据tfp设置trace追踪的层次
      Vtop *top = new Vtop("top"); 		  //指向Vtop类的 top地址的指针，new分配内存空间并且调用Vtop的构造函数，新建了一个对象（类）top
-     
-     top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
-    // tfp->dumpvars(1, "t");  // trace 1 level under "t"
-     tfp->open("test.vcd");
-  /////////////////////////////产生波形固定内容
-
-    // while (sc_time_stamp() < 20 && !Verilated::gotFinish()) {                                                                                                                                            
-  while (1) {
+     top->trace(tfp, 99);  			  // Trace 99 levels of hierarchy (or see below)
+    // tfp->dumpvars(1, "t");  			  // trace 1 level under "t"
+     tfp->open("test.vcd"); 
+     ///////////////////////////固定内容 verilator init////////////////////////////////
+  
+    // while (sc_time_stamp() < 20 && !Verilated::gotFinish())                                                                                                                                        
+    while (1) {
   	  nvboard_update();
 	  single_cycle();
+
+         /////////////////////////仿真赋值用///////////////////////////////////
          // int a = rand() & 1;
          // int b = rand() & 1;
          //  top->a = a;
          //  top->b = b;      //top指针操作，赋值a，b
-          top->eval();    //执行一次erilog代码 更新值
+       
+        ////////////////////////步进执行并产生波形；更新电路状态/////////////////////////////
+        top->eval();    //执行一次erilog代码 更新值
 	// tfp->dump(main_time);
         // main_time++;                                            //更新仿真时间
-        // printf("sw0 = %d, sw1 = %d, ld0 = %d\n", a, b, f);       //看看而已
+        ////////////////////////步进执行并产生波形；更新电路状态////////////////////////////
+
+	////////////////////检测查看用//////////////////////////////////////
+	// printf("sw0 = %d, sw1 = %d, ld0 = %d\n", a, b, f);       //看看而已
         //  assert(top->f == a ^ b);                                //断言检测 
-          
-  
        }
+
+       //退出nvboard 和 verilator 若不用 第一个while限制次数，则不会走到这
      	  nvboard_quit();
           top->final();
           tfp->close();
           delete top;
-          
           return 0;
   }   
   
---
+++ npc/vsrc/historyfile/mux/mux41.v
@@ -0,0 +1,37 @@
+ module mux41(                                                                                                                                                                                            
+ //  input  [7:0] x,
+ // input  [1:0] y,
+ //  output [1:0] f
+ input           rst,
+ input           clk,
+ input   [7:0]   sw,
+ output  [15:0]  ledr
+   );
+ 
+ wire  [7:0]     x;//四个两位输入信号
+ wire  [1:0]     y;//使能key信号
+ wire  [1:0]     f;//输出信号
+ reg             clk_led;
+ always@(posedge clk)begin
+         if(rst)begin
+                 clk_led <= 1'b1;
+                 end
+         else begin
+                 clk_led<= 1'b0;
+                 end
+ end
+   MuxKeyWithDefault #(4, 2, 2)
+   inst  (f,
+         y,
+         2'b11,
+         { 2'b00, x[1:0],
+           2'b01, x[3:2],
+           2'b10, x[5:4],
+           2'b11, x[7:6]
+         });
+   assign  ledr[15:0] = {clk_led,{13{1'b1}},f[1:0]};
+   assign  y[1:0]     = sw[1:0];
+   assign  x[7:0]     = {1'b0,1'b1,sw[7:2]};
+ 
+ endmodule
+
