02f3c7699a3c1d8dd787eb794a382a6d23f4ed3f
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 21:01:00 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:01:00 up 1 day,  1:00,  1 user,  load average: 0.16, 0.07, 0.08

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,18 +1,26 @@
 module top(
-  input clk,
-  input rst,
-  output reg [15:0] ledr
-);
-  reg [31:0] count;
-  reg [15:0] led;  
-  always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]};
-      count <= (count >= 5000000 ? 32'b0 : count + 1);
-    end
-  end
-  assign  ledr = {led[15:0]};
+//  input  [7:0] x,
+// input  [1:0] y,
+//  output [1:0] f
+input	[7:0]	sw,
+output  [15:0]	ledr
+  );
+
+wire  [7:0]	x;//四个两位输入信号
+wire  [1:0]	y;//使能key信号
+wire  [1:0]     f;//输出信号
+
+  MuxKeyWithDefault #(4, 2, 2) 
+  inst  (f, 
+  	y, 
+  	2'b11, 
+  	{ 2'b00, x[1:0],
+    	  2'b01, x[3:2],
+    	  2'b10, x[5:4],
+    	  2'b11, x[7:6]
+  	});
+  assign  ledr[15:0] = {14{1'b1},f[1:0]};
+  assign  y[1:0]     = sw[1:0];
+  assign  x[7:0]     = {1'b0,1'b1,sw[7:2]};
 
 endmodule
-
