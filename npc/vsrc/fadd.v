e82f7b6e874cac1e3c5929295bbc9e9a063e0ab1
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 18:44:32 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:44:32 up 1 day, 22:44,  1 user,  load average: 0.28, 0.58, 0.78

--- npc/vsrc/fadd.v
+++ npc/vsrc/fadd.v
@@ -9,18 +9,14 @@ module fadd (
  );
   reg	 [4:0]   c;//分别对应Gi、Pi和Ci
   wire   [3:0]   g,p;
+  wire	 	 sh,ch,sl,cl;// 最高位和次高位，不考虑下级进位情况下的 和 与 进位
   
- /* assign  c[0] = cin;//最低进位为输入进位
-  assign  p = a | b;// Pi = Ai+Bi
-  assign  g = a & b;// Gi = Ai.Bi
-  assign  c[1] = g[0] | (p[0] & c[0]);//对应图中 C1
-  assign  c[2] = g[1] | (p[1] & (g[0] | (p[0] & c[0])));
-  assign  c[3] = g[2] | (p[2] &(g[1] | (p[1] &(g[0]|(p[0]&c[0])))));
-  assign  c[4] = g[3] | (p[3] &(g[2] | (p[2] &(g[1]|(p[1]&(g[0]|(p[0]&c[0])))))));*/
-  assign  overflow = (c[4] == c[3]) ? 1'b0 : 1'b1;
+  assign  {ch,sh} = a[3]+b[3];
+  assign  {cl,sl} = a[2]+b[2];
+  assign  overflow = (ch == cl) ? 1'b0 : 1'b1;
+  
   assign  p = a | b;// Pi = Ai+Bi   
   assign  g = a & b;// Gi = Ai.Bi
-
   always@(posedge clk) begin
 	if(rst)	begin
 	c <= 5'd0;
