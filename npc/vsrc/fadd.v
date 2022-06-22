47d482cb75e77f3ea68189a4bdd1f99d38c9b056
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 17:09:51 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:09:51 up 1 day, 21:09,  1 user,  load average: 0.05, 0.12, 0.13

--- npc/vsrc/fadd.v
+++ npc/vsrc/fadd.v
@@ -1,18 +1,36 @@
 module fadd ( 
-  output [3:0]sum;//数据累加和本位
-  output		  cout;//输出进位
-  input  [3:0]a,b;//需要相加的数
-  input       cin;//输入进位
+  output reg[3:0]	sum,//数据累加和本位
+  output reg		cout,//输出进位
+  output 		overflow,
+  input     [3:0]	a,b,//需要相加的数
+  input       		cin,//输入进位
+  input 		clk,
+  input			rst
  );
   wire	 [3:0]g,p,c;//分别对应Gi、Pi和Ci
   
-  assign  c[0] = din;//最低进位为输入进位
-  assign  P = a | b;// Pi = Ai·Bi
-  assign  g = a & b;// Gi = Ai+Bi
+  assign  c[0] = cin;//最低进位为输入进位
+  assign  P = a | b;// Pi = Ai+Bi
+  assign  g = a & b;// Gi = Ai.Bi
   assign  c[1] = g[0] | (p[0] & c[0]);//对应图中 C1
   assign  c[2] = g[1] | (p[1] &(g[0] | (p[0] & c[0]) );
   assign  c[3] = g[2] | (p[2] &(g[1] | (p[1] &(g[0]|(p[0]&c[0])))));
   assign  c[4] = g[3] | (p[3] &(g[2] | (p[2] &(g[1]|(p[1]&(g[0]|(p[0]&c[0])))))));
-  assign  sum = p ^ c[3:0];  
-  assign  cout = c[4];
+  assign  overflow = (c[4] == c[3]) ? 1'b0 : 1'b1;
+
+  always@(posedge clk) begin
+	if(rst)	begin
+	sum	<= 4'd0;
+	cout	<= 1'b0; 
+		end
+	else	begin
+	sum	<= p ^ c[3:0]; 
+	cout	<= c[4];
+		end
+  end
+
+
+ /* assign  sum = p ^ c[3:0];  
+  assign  cout = c[4];*/
+
 endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,51 +1,44 @@
 module top(
 input 		rst,
 input 		clk,
-input	[7:0]	sw,
+input	[15:0]	sw,
 output  [15:0]	ledr,
-output  [7:0] 	seg0,
-/*output  [7:0] 	seg1,
-output  [7:0] 	seg2,
-output  [7:0] 	seg3,
-output  [7:0] 	seg4,
-output  [7:0] 	seg5,
-output  [7:0] 	seg6,*/
-output  [7:0] 	seg7
-
   );
+  
+wire[2:0]	func;
+wire		overflow;
+wire[3:0]	sum;
+wire		cout;
+wire[3:0]	logic;
+wire		zero;
+wire[3:0]	a;
+wire[3:0]       b;
 
-reg	[2:0]	decimal;
-wire		flag;
-//assign	decimal = 3'd6; //test
-
-  always@(sw) begin
-	casez(sw) 
-	8'b1???????: decimal = 3'd7;
-	8'b01??????: decimal = 3'd6;
-	8'b001?????: decimal = 3'd5;
-	8'b0001????: decimal = 3'd4;
-	8'b00001???: decimal = 3'd3;
-	8'b000001??: decimal = 3'd2;
-	8'b0000001?: decimal = 3'd1;
-	8'b00000001: decimal = 3'd0;
-	default:     decimal = 3'd0;
+assign  zero = ~(|sum);
+assign	func = sw[10:8];
+assign	ledr = {{6{1'b0}},logci[3:0],overflow,cout,sum[3:0]};
+always@(func) begin
+	case(func)
+	3'b000:begin a=sw[3:0]; b=sw[7:4]; end //+
+	3'b001:begin a=sw[3:0]; b=sw[7:4]; end //-
+	3'b010:begin a=4'd0; b=4'd0; logic= ~sw[3:0];          end //~
+  	3'b011:begin a=4'd0; b=4'd0; logic= sw[3:0] & sw[7:4]; end //&
+  	3'b100:begin a=4'd0; b=4'd0; logic= sw[3:0] | sw[7:4]; end //|
+	3'b101:begin a=4'd0; b=4'd0; logic= sw[3:0] ^ sw[7:4]; end // xor
+  	3'b110:begin a=sw[3:0]; b=sw[7:4]; logic= sum[3] ^ overflow; end //a<b则为1 ；>则为0
+  	3'b111:begin a=sw[3:0]; b=sw[7:4]; logic[0]=zero;  end //相等则为1；
+	default:begin a=4'd0; b=4'd0; logic=4'd0;	end
 	endcase
-  end
-  assign flag = (sw == 8'd0) ? 1'b0 : 1'b1;
-  assign ledr[15:0] = {{12{1'b0}},flag,decimal[2:0]};
+end
 
-  seg seg_inst(
+  fadd fadd_inst(
+	.sum		(sum),
+	.cout		(cout),
+	.overflow	(overflow),
+	.a		(a),
+	.b		(b),
+	.cin		(1'b0),
 	.clk		(clk),
-	.rst		(rst),
-	.decimal	(decimal),
-	.o_seg0		(seg0),
-/*	.o_seg1		(seg1),
-	.o_seg2		(seg2),
-	.o_seg3		(seg3),
-	.o_seg4		(seg4),
-	.o_seg5		(seg5),
-	.o_seg6		(seg6),*/
-	.o_seg7		(seg7) );
-
+	.rst		(rst));
 
 endmodule
