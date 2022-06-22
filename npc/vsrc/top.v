211bd2d3e2f1d2c55f7b775068b7d9688ed9a1be
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 17:59:33 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:59:33 up 1 day, 21:59,  1 user,  load average: 0.47, 0.38, 0.22

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,7 @@
 module top(
 input 		rst,
 input 		clk,
-input	[15:0]	sw,
+input	[10:0]	sw,
 output  [15:0]	ledr
   );
   
@@ -13,12 +13,9 @@ wire		zero;
 reg[3:0]	a;
 reg[3:0]        b;
 reg[3:0]        f;
-wire[4:0]	        rongyu;
-assign	rongyu = sw[15:11];
 assign  zero = ~(|sum);
 assign	func[2:0] = sw[10:8];
 assign	ledr[15:0] = {{6{1'b0}},f[3:0],overflow,cout,sum[3:0]};
-//assign        ledr = {{10{1'b0}},overflow,cout,sum[3:0]};
 always@(func) begin
 	case(func)
 	3'b000:begin a=sw[3:0]; b=sw[7:4]; f[3:0] = 4'b0000; end //+
