b1cdf1ad0c0747cdbf951148be1c13525bee73eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 17:37:19 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:37:19 up 1 day, 21:37,  1 user,  load average: 0.15, 0.15, 0.13

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -9,19 +9,19 @@ wire[2:0]	func;
 wire		overflow;
 wire[3:0]	sum;
 wire		cout;
-//wire[3:0]	logic;
+wire[3:0]	logic;
 wire		zero;
 wire[3:0]	a;
 wire[3:0]       b;
 
 assign  zero = ~(|sum);
 assign	func = sw[10:8];
-//assign	ledr = {{6{1'b0}},logci[3:0],overflow,cout,sum[3:0]};
-assign        ledr = {{10{1'b0}},overflow,cout,sum[3:0]};
+assign	ledr = {{6{1'b0}},{logci[3:0]},{overflow},{cout},{sum[3:0]}};
+//assign        ledr = {{10{1'b0}},overflow,cout,sum[3:0]};
 always@(func) begin
 	case(func)
-	3'b000:begin a=sw[3:0]; b=sw[7:4]; logic=4'd0; end //+
-	3'b001:begin a=sw[3:0]; b=sw[7:4]; logic=4'd0; end //-
+	3'b000:begin a=sw[3:0]; b=sw[7:4]; logic[3:0] = 4'd0; end //+
+	3'b001:begin a=sw[3:0]; b=sw[7:4]; logic[3:0] = 4'd0; end //-
 	3'b010:begin a=4'd0; b=4'd0; logic[3:0] = ~sw[3:0];          end //~
   	3'b011:begin a=4'd0; b=4'd0; logic[3:0] = sw[3:0] & sw[7:4]; end //&
   	3'b100:begin a=4'd0; b=4'd0; logic[3:0] = sw[3:0] | sw[7:4]; end //|
