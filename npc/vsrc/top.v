56c05c21a3f71c3db69f5d10dde69bb7881531c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 17:51:54 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:51:53 up 1 day, 21:51,  1 user,  load average: 0.01, 0.06, 0.08

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -12,23 +12,23 @@ wire		cout;
 wire		zero;
 wire[3:0]	a;
 wire[3:0]       b;
-wire[3:0]       logic;
+wire[3:0]       f;
 
 assign  zero = ~(|sum);
 assign	func[2:0] = sw[10:8];
-assign	ledr[15:0] = {{6{1'b0}},logic[3:0],overflow,cout,sum[3:0]};
+assign	ledr[15:0] = {{6{1'b0}},f[3:0],overflow,cout,sum[3:0]};
 //assign        ledr = {{10{1'b0}},overflow,cout,sum[3:0]};
 always@(func) begin
 	case(func)
-	3'b000:begin a=sw[3:0]; b=sw[7:4]; logic[3:0] = 4'b0000; end //+
-	3'b001:begin a=sw[3:0]; b=sw[7:4]; logic[3:0] = 4'b0000; end //-
-	3'b010:begin a=4'd0; b=4'd0; logic[3:0] = ~sw[3:0];          end //~
-  	3'b011:begin a=4'd0; b=4'd0; logic[3:0] = sw[3:0] & sw[7:4]; end //&
-  	3'b100:begin a=4'd0; b=4'd0; logic[3:0] = sw[3:0] | sw[7:4]; end //|
-	3'b101:begin a=4'd0; b=4'd0; logic[3:0] = sw[3:0] ^ sw[7:4]; end // xor
-  	3'b110:begin a=sw[3:0]; b=sw[7:4]; logic[0] = sum[3] ^ overflow; end //a<b则为1 ；>则为0
-  	3'b111:begin a=sw[3:0]; b=sw[7:4]; logic[0] =zero;  end //相等则为1；
-	default:begin a=4'd0; b=4'd0; logic=4'b0000;	end
+	3'b000:begin a=sw[3:0]; b=sw[7:4]; f[3:0] = 4'b0000; end //+
+	3'b001:begin a=sw[3:0]; b=sw[7:4]; f[3:0] = 4'b0000; end //-
+	3'b010:begin a=4'd0; b=4'd0; f[3:0] = ~sw[3:0];          end //~
+  	3'b011:begin a=4'd0; b=4'd0; f[3:0] = sw[3:0] & sw[7:4]; end //&
+  	3'b100:begin a=4'd0; b=4'd0; f[3:0] = sw[3:0] | sw[7:4]; end //|
+	3'b101:begin a=4'd0; b=4'd0; f[3:0] = sw[3:0] ^ sw[7:4]; end // xor
+  	3'b110:begin a=sw[3:0]; b=sw[7:4]; f[0] = sum[3] ^ overflow; end //a<b则为1 ；>则为0
+  	3'b111:begin a=sw[3:0]; b=sw[7:4]; f[0] =zero;  end //相等则为1；
+	default:begin a=4'd0; b=4'd0; f[3:0]=4'b0000;	end
 	endcase
 end
 
