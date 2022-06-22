06b3d147718ba098d6e27999475f487461bf4417
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 00:01:33 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:01:33 up 2 days,  4:01,  1 user,  load average: 0.13, 0.06, 0.18

--- npc/vsrc/Barrel_shifter.v
+++ npc/vsrc/Barrel_shifter.v
@@ -6,32 +6,32 @@ input[1:0]	shift_ctl,
 output reg [7:0]	shift_out
 );
 
- reg [7:0]	temp;
-// reg [7:0]      temp1;
-// reg [7:0]      temp2;
+ reg [7:0]	temp0;
+ reg [7:0]      temp1;
+ reg [7:0]      temp2;
 
  always@(shift_in or shift_num or shift_ctl)begin
-	shift_out = temp;
+	shift_out = temp2;
 		case(shift_ctl)
 		2'b00:begin 
-		temp = shift_num[0] ? {shift_in[6:0],1'b0} : shift_in;
-		temp = shift_num[1] ? {temp[5:0],{2{1'b0}}} : temp;
-		temp = shift_num[2] ? {temp[3:0],{4{1'b0}}} : temp;
+		temp0 = shift_num[0] ? {shift_in[6:0],1'b0} : shift_in;
+		temp1 = shift_num[1] ? {temp0[5:0],{2{1'b0}}} : temp0;
+		temp2 = shift_num[2] ? {temp1[3:0],{4{1'b0}}} : temp1;
 		end // a/l shift left
 		2'b01:begin 
-		temp = shift_num[0] ? {1'b0,shift_in[7:1]} : shift_in;
-		temp = shift_num[1] ? {{2{1'b0}},temp[7:2]} : temp;
- 		temp = shift_num[2] ? {{4{1'b0}},temp[7:4]} : temp;
+		temp0 = shift_num[0] ? {1'b0,shift_in[7:1]} : shift_in;
+		temp1 = shift_num[1] ? {{2{1'b0}},temp0[7:2]} : temp0;
+ 		temp2 = shift_num[2] ? {{4{1'b0}},temp1[7:4]} : temp1;
 		end // l shift right
 		2'b10:begin 
-		temp = shift_num[0] ? {shift_in[7],shift_in[7:1]} : shift_in;      
-		temp = shift_num[1] ? {temp[7],1'b0,temp[7:2]} : temp;
-		temp = shift_num[2] ? {temp[7],{3{1'b0}},temp[7:4]} : temp; 
+		temp0 = shift_num[0] ? {shift_in[7],shift_in[7:1]} : shift_in;      
+		temp1 = shift_num[1] ? {temp0[7],1'b0,temp0[7:2]} : temp0;
+		temp2 = shift_num[2] ? {temp1[7],{3{1'b0}},temp1[7:4]} : temp1; 
 		end // a shift right
 		2'b11:begin 
-		temp = shift_num[0] ? {shift_in[0],shift_in[7:1]} : shift_in;   
-		temp = shift_num[1] ? {temp[1:0],temp[7:2]} : temp;
-		temp = shift_num[2] ? {temp[3:0],temp[7:4]} : temp;
+		temp0 = shift_num[0] ? {shift_in[0],shift_in[7:1]} : shift_in;   
+		temp1 = shift_num[1] ? {temp0[1:0],temp0[7:2]} : temp0;
+		temp2 = shift_num[2] ? {temp1[3:0],temp1[7:4]} : temp1;
 		end // rotate  right
 		default:begin	shift_out = shift_in;  				end
 		endcase	
