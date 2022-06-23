69882913b09aae3454dae91f3958cb2dc496a40e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 19:34:43 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:34:43 up 2 days, 23:34,  1 user,  load average: 0.35, 0.50, 0.41

--- npc/build/auto_bind.cpp
+++ npc/build/auto_bind.cpp
@@ -7,4 +7,6 @@ void nvboard_bind_all_pins(Vtop* top) {
 	nvboard_bind_pin( &top->seg1, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG1A, SEG1B, SEG1C, SEG1D, SEG1E, SEG1F, SEG1G, DEC1P);
 	nvboard_bind_pin( &top->seg4, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG4A, SEG4B, SEG4C, SEG4D, SEG4E, SEG4F, SEG4G, DEC4P);
 	nvboard_bind_pin( &top->seg5, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG5A, SEG5B, SEG5C, SEG5D, SEG5E, SEG5F, SEG5G, DEC5P);
+	nvboard_bind_pin( &top->ps2_clk, BIND_RATE_RT , BIND_DIR_IN , 1, PS2_CLK);
+	nvboard_bind_pin( &top->ps2_data, BIND_RATE_RT , BIND_DIR_IN , 1, PS2_DAT);
 }
