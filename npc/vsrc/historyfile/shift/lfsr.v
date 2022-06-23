2947b605cc89fbf2bb0c2aef6091b89344323e46
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 19:11:16 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:11:16 up 2 days, 23:11,  1 user,  load average: 0.15, 0.11, 0.09

---
+++ npc/vsrc/historyfile/shift/lfsr.v
@@ -0,0 +1,61 @@
+
+module lfsr(
+input 			rst,
+input 			clk,
+input  [9:0]		sw,//en,clk,seed_datas
+output [15:0]		ledr,//flag,zero_flag,indentify
+output reg[7:0] 		seg0,//l 4bit
+output reg[7:0] 		seg1 //h 4bit
+  );
+  
+ reg		led_flag;
+ wire [4:0]	led_zero;
+ reg  [7:0]	lfsr;
+ wire           h_xor;
+ wire [7:0] 	segs [8:0];
+ 
+ assign segs[0] = 8'b11111101;
+ assign segs[1] = 8'b01100000;
+ assign segs[2] = 8'b11011010;
+ assign segs[3] = 8'b11110010;
+ assign segs[4] = 8'b01100110;
+ assign segs[5] = 8'b10110110;
+ assign segs[6] = 8'b10111110;
+ assign segs[7] = 8'b11100000;
+ assign segs[8] = 8'b11111111;
+ assign	ledr[15:0] = {led_flag,led_zero[4:0],sw[9:0]};
+ assign led_zero   = (lfsr[7:0]==8'd0) ? 5'b11111 : 5'b00000;
+ assign h_xor = sw[9] ? (sw[0]^sw[2]^sw[3]^sw[4]) : (lfsr[0] ^ lfsr[2] ^ lfsr[3] ^ lfsr[4]);
+ always@(posedge sw[8])	begin
+	if(sw[9])begin
+	lfsr  <= sw[7:0];
+//	h_xor <= sw[0] ^ sw[2] ^ sw[3] ^ sw[4]; 
+	end
+	else begin
+	lfsr  <= {h_xor,lfsr[7:1]};
+//	h_xor <= lfsr[0] ^ lfsr[2] ^ lfsr[3] ^ lfsr[4];
+	end
+ end
+
+ always@(lfsr) begin
+	case(lfsr)
+	8'b0000_0001:begin  seg0 = ~segs[1];  seg1 = ~segs[0]; end
+	8'b1000_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[8]; end
+	8'b0100_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[4]; end
+	8'b0010_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[2]; end
+	8'b0001_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[1]; end
+	8'b1000_1000:begin  seg0 = ~segs[8];  seg1 = ~segs[8]; end
+	default:begin  seg0 = ~segs[0]; seg1 = ~segs[0];end
+	endcase
+ end
+
+ always@(posedge clk)begin
+	if(rst)begin
+	led_flag <= 1'b0;
+	end
+	else begin
+	led_flag <= 1'b0;
+	end
+ end
+
+endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,18 +1,53 @@
 module top(
 input 			rst,
 input 			clk,
-input  [9:0]		sw,//en,clk,seed_datas
+input 			ps2_clk,
+input 			ps2_data,
 output [15:0]		ledr,//flag,zero_flag,indentify
 output reg[7:0] 		seg0,//l 4bit
 output reg[7:0] 		seg1 //h 4bit
   );
   
- reg		led_flag;
- wire [4:0]	led_zero;
- reg  [7:0]	lfsr;
- wire           h_xor;
+ wire [12:0]	led_flag;
  wire [7:0] 	segs [8:0];
+ reg		nextdata_n;
+ wire [7:0]	data;
+ reg  [7:0]	mc;
+ wire		ready, overflow;
+ reg  [2:0]	cnt;
  
+ assign ledr[15:0]  = {led_flag[12:0],overflow,ready,nextdata_n};
+
+ //键盘控制器 
+ always@(posedge clk)	begin
+	if(rst)begin
+	nextdata_n  <= 1'b1;
+	mc	    <= 8'd0;
+	cnt	    <= 3'd0;
+	end
+	else if(ready) begin
+	mc          <= data;
+	nextdata_n  <= 1'b0;
+	cnt	    <= cnt+1'b1;
+	end
+	else begin
+	mc          <= 8'd0;
+	nextdata_n  <= 1'b1;
+	cnt	    <= cnt;
+	end
+ end
+
+ps2_keyboard my_keyboard(
+    .clk		(clk),
+    .resetn		(~rst),
+    .ps2_clk		(ps2_clk),
+    .ps2_data		(ps2_data),
+    .nextdata_n		(nextdata_n),
+    .data		(data),
+    .ready		(ready),
+    .overflow		(overflow));
+
+ //数码管
  assign segs[0] = 8'b11111101;
  assign segs[1] = 8'b01100000;
  assign segs[2] = 8'b11011010;
@@ -22,38 +57,31 @@ output reg[7:0] 		seg1 //h 4bit
  assign segs[6] = 8'b10111110;
  assign segs[7] = 8'b11100000;
  assign segs[8] = 8'b11111111;
- assign	ledr[15:0] = {led_flag,led_zero[4:0],sw[9:0]};
- assign led_zero   = (lfsr[7:0]==8'd0) ? 5'b11111 : 5'b00000;
- assign h_xor = sw[9] ? (sw[0]^sw[2]^sw[3]^sw[4]) : (lfsr[0] ^ lfsr[2] ^ lfsr[3] ^ lfsr[4]);
- always@(posedge sw[8])	begin
-	if(sw[9])begin
-	lfsr  <= sw[7:0];
-//	h_xor <= sw[0] ^ sw[2] ^ sw[3] ^ sw[4]; 
-	end
-	else begin
-	lfsr  <= {h_xor,lfsr[7:1]};
-//	h_xor <= lfsr[0] ^ lfsr[2] ^ lfsr[3] ^ lfsr[4];
-	end
- end
-
- always@(lfsr) begin
-	case(lfsr)
-	8'b0000_0001:begin  seg0 = ~segs[1];  seg1 = ~segs[0]; end
-	8'b1000_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[8]; end
-	8'b0100_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[4]; end
+ always@(mc) begin
+	case(mc)
+	8'b0001_0101:begin  seg0 = ~segs[5];  seg1 = ~segs[1]; end//q ,15h
+	8'b0010_0011:begin  seg0 = ~segs[3];  seg1 = ~segs[2]; end//d, 23h
+/*	8'b0100_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[4]; end
 	8'b0010_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[2]; end
 	8'b0001_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[1]; end
-	8'b1000_1000:begin  seg0 = ~segs[8];  seg1 = ~segs[8]; end
-	default:begin  seg0 = ~segs[0]; seg1 = ~segs[0];end
+	8'b1000_1000:begin  seg0 = ~segs[8];  seg1 = ~segs[8]; end*/
+	default:begin  seg0 = ~segs[8]; seg1 = ~segs[8];end
+	endcase
+	case(cnt)
+	3'd0:begin   seg5 = ~segs[0];  seg4 = ~segs[0]; end
+	3'd1:begin   seg5 = ~segs[0];  seg4 = ~segs[1]; end
+	3'd2:begin   seg5 = ~segs[0];  seg4 = ~segs[2]; end
+	3'd3:begin   seg5 = ~segs[0];  seg4 = ~segs[3]; end
+	default:begin seg5 = ~segs[8];  seg4 = ~segs[8]; end
 	endcase
  end
 
  always@(posedge clk)begin
 	if(rst)begin
-	led_flag <= 1'b0;
+	led_flag <= 12'd0;
 	end
 	else begin
-	led_flag <= 1'b0;
+	led_flag <= 12'd0;
 	end
  end
 
