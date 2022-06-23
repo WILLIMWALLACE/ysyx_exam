0186036cad76a5582bc230f4d082c7acadf0e3b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-23 19:53:10 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:53:10 up 2 days, 23:52,  1 user,  load average: 0.25, 0.66, 0.57

--- npc/csrc/top_tb.cpp
+++ npc/csrc/top_tb.cpp
@@ -37,7 +37,7 @@
      VerilatedVcdC*  tfp = new  VerilatedVcdC;    //导出波形需用，且可根据tfp设置trace追踪的层次
      Vtop *top = new Vtop("top"); 		  //指向Vtop类的 top地址的指针，new分配内存空间并且调用Vtop的构造函数，新建了一个对象（类）top
      top->trace(tfp, 99);  			  // Trace 99 levels of hierarchy (or see below)
-    // tfp->dumpvars(1, "t");  			  // trace 1 level under "t"
+     tfp->dumpvars(1, "t");  			  // trace 1 level under "t"
      tfp->open("test.vcd"); 
      ///////////////////////////固定内容 verilator init////////////////////////////////
   
@@ -54,8 +54,8 @@
         
         ////////////////////////步进执行并产生波形；更新电路状态/////////////////////////////
         top->eval();    //执行一次erilog代码 更新值
-	// tfp->dump(main_time);
-        // main_time++;                                            //更新仿真时间
+	 tfp->dump(main_time);
+         main_time++;                                            //更新仿真时间
         ////////////////////////步进执行并产生波形；更新电路状态////////////////////////////
 
 	////////////////////检测查看用//////////////////////////////////////
