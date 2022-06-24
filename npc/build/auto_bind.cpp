325c0f83f99fb32142ec2b0d2bf91105a697b56d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 14:53:54 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:53:54 up 3 days, 18:53,  1 user,  load average: 0.14, 0.17, 0.12

--- npc/build/auto_bind.cpp
+++ npc/build/auto_bind.cpp
@@ -2,6 +2,12 @@
 #include "Vtop.h"
 
 void nvboard_bind_all_pins(Vtop* top) {
+	nvboard_bind_pin( &top->VGA_VSYNC, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_VSYNC);
+	nvboard_bind_pin( &top->VGA_HSYNC, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_HSYNC);
+	nvboard_bind_pin( &top->VGA_BLANK_N, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_BLANK_N);
+	nvboard_bind_pin( &top->VGA_R, BIND_RATE_RT , BIND_DIR_OUT, 8, VGA_R7, VGA_R6, VGA_R5, VGA_R4, VGA_R3, VGA_R2, VGA_R1, VGA_R0);
+	nvboard_bind_pin( &top->VGA_G, BIND_RATE_RT , BIND_DIR_OUT, 8, VGA_G7, VGA_G6, VGA_G5, VGA_G4, VGA_G3, VGA_G2, VGA_G1, VGA_G0);
+	nvboard_bind_pin( &top->VGA_B, BIND_RATE_RT , BIND_DIR_OUT, 8, VGA_B7, VGA_B6, VGA_B5, VGA_B4, VGA_B3, VGA_B2, VGA_B1, VGA_B0);
 	nvboard_bind_pin( &top->ledr, BIND_RATE_SCR, BIND_DIR_OUT, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
 	nvboard_bind_pin( &top->seg0, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G, DEC0P);
 	nvboard_bind_pin( &top->seg1, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG1A, SEG1B, SEG1C, SEG1D, SEG1E, SEG1F, SEG1G, DEC1P);
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -4,13 +4,15 @@ input 			clk,
 input 			ps2_clk,
 input 			ps2_data,
 output [15:0]		ledr,
-output reg[7:0] 		seg0,
-output reg[7:0] 		seg1,
-output reg[7:0]                 seg4,
-output reg[7:0]                 seg5
-
+output 			VGA_CLK,
+output 			VGA_HSYNC,
+output 			VGA_VSYNC,
+output 			VGA_BLANK_N,
+output [7:0] 		VGA_R,
+output [7:0] 		VGA_G,
+output [7:0] 		VGA_B
   );
-  
+ // ps2键盘输入控制所需信号  
  reg  [7:0]	led_flag;
  wire [7:0] 	segs [8:0];
  reg		nextdata_n;
@@ -24,10 +26,33 @@ output reg[7:0]                 seg5
  reg  [2:0]	c_state;
  reg  [2:0]     n_state;
  reg		flag;//切换 一个按键按下来并松开的三种情况
- assign ledr[15:0]  = {led_flag[7:0], sampling, count[3:0], overflow, ready, nextdata_n};
- 
- //键盘控制器 
 
+ //vga控制所需信号
+  wire [9:0] h_addr;
+  wire [9:0] v_addr;
+  wire [23:0] vga_data;
+  assign VGA_CLK = clk;
+  vga vga_ctrl(
+    .pclk(clk),
+    .reset(rst),
+    .vga_data(vga_data),
+    .h_addr(h_addr),
+    .v_addr(v_addr),
+    .hsync(VGA_HSYNC),
+    .vsync(VGA_VSYNC),
+    .valid(VGA_BLANK_N),
+    .vga_r(VGA_R),
+    .vga_g(VGA_G),
+    .vga_b(VGA_B)
+  );
+  //读取 图片数据并 转换成vga数据格式
+  vmem my_vmem(
+    .h_addr(h_addr),
+    .v_addr(v_addr[8:0]),
+    .vga_data(vga_data)
+  );
+
+ //键盘控制器 ,二段式状态机控制+键盘输入缓冲模块
   always@(posedge clk)   begin
         if(rst)begin
        	c_state <= 3'd0;
@@ -70,25 +95,7 @@ output reg[7:0]                 seg5
 	end
   end
 
-/*  always@(posedge clk)	begin
-	if(rst)begin
-	nextdata_n  <= 1'b0;
-	mc	    <= 8'd0;
-	cnt	    <= 3'd0;
-	end
-	else if(ready) begin
-	mc          <= data;
-	nextdata_n  <= 1'b0;
-	cnt	    <= cnt+1'b1;
-	end
-	else begin
-	mc          <= 8'd0;
-	nextdata_n  <= 1'b1;
-	cnt	    <= cnt;
-	end
- end*/
-
-ps2_keyboard my_keyboard(
+  ps2_keyboard my_keyboard(
     .clk		(clk),
     .resetn		(~rst),
     .ps2_clk		(ps2_clk),
@@ -98,39 +105,9 @@ ps2_keyboard my_keyboard(
     .ready		(ready),
     .overflow		(overflow),
     .sampling		(sampling),
-    .count		(count)
-
-    );
-
- //数码管
- assign segs[0] = 8'b11111101;
- assign segs[1] = 8'b01100000;
- assign segs[2] = 8'b11011010;
- assign segs[3] = 8'b11110010;
- assign segs[4] = 8'b01100110;
- assign segs[5] = 8'b10110110;
- assign segs[6] = 8'b10111110;
- assign segs[7] = 8'b11100000;
- assign segs[8] = 8'b11111111;
- always@(mc or cnt) begin
-	case(mc)
-	8'b0001_0101:begin  seg0 = ~segs[5];  seg1 = ~segs[1]; end//q ,15h
-	8'b0010_0011:begin  seg0 = ~segs[3];  seg1 = ~segs[2]; end//d, 23h
-/*	8'b0100_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[4]; end
-	8'b0010_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[2]; end
-	8'b0001_0000:begin  seg0 = ~segs[0];  seg1 = ~segs[1]; end
-	8'b1000_1000:begin  seg0 = ~segs[8];  seg1 = ~segs[8]; end*/
-	default:begin  seg0 = ~segs[8]; seg1 = ~segs[8];end
-	endcase
-	case(cnt)
-	3'd0:begin   seg5 = ~segs[0];  seg4 = ~segs[0]; end
-	3'd1:begin   seg5 = ~segs[0];  seg4 = ~segs[1]; end
-	3'd2:begin   seg5 = ~segs[0];  seg4 = ~segs[2]; end
-	3'd3:begin   seg5 = ~segs[0];  seg4 = ~segs[3]; end
-	default:begin seg5 = ~segs[8];  seg4 = ~segs[8]; end
-	endcase
- end
-
+    .count		(count));
+ //led指示灯
+ assign ledr[15:0]  = {led_flag[7:0], sampling, count[3:0], overflow, ready, nextdata_n};
  always@(posedge clk)begin
 	if(rst)begin
 	led_flag <= 8'd0;
@@ -139,5 +116,4 @@ ps2_keyboard my_keyboard(
 	led_flag <= 8'd0;
 	end
  end
-
 endmodule
