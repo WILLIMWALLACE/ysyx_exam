a4e5712d82e7d2d3de3ed27cf0bdd79b2251411c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 09:55:44 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:55:44 up 55 min,  1 user,  load average: 0.07, 0.25, 0.26

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -67,7 +67,7 @@ static int cmd_x(char *args) {
     num = atoi(strtok(NULL, " "));//"1234"->1234 ; char(shu zi) -> int
     exp = strtok(NULL, " ");//strtok(char,"fen ge biao zhi")shou ci yong char s,zhi hou yong NULL
     addr = strtol(exp,NULL,16);
-    printf("%d\n%s\n%x\n",num,exp,addr);
+    //printf("%d\n%s\n%x\n",num,exp,addr);
     //paddr = addr;
     for (i = 0; i < num; i++) {
       printf("0x%lx\n", paddr_read(addr, 4));
