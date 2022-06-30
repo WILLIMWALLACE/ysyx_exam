b17b4f0725e8743d94f5d26245ec518f9a02f1d1
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 17:09:50 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:09:50 up 2 days,  8:09,  1 user,  load average: 0.08, 0.30, 0.39

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -11,13 +11,38 @@ static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned result = %s; "
+"  unsigned result = gen_rand_expr(); "
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
 
+static int choose(int n){
+  u_int32_t seed=time(NULL);
+  srand(seed);
+  return rand()%n;
+}
+
+static int gen_op(){
+  int branch=choose(4);
+  switch(branch){
+    case 0: return  '+'; break;
+    case 1: return  '-'; break;
+    case 2: return  '*'; break;
+    case 3: return  '/'; break;
+    default:assert(0);
+  }
+}
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  //buf[0] = '\0';
+  int i;
+    for(i=0;i<100;){
+    switch(choose(3)){
+      case 0:   buf[i]=choose(100); i++; break; //gen_num
+      case 1:   buf[i]='('; buf[i+1]=;buf[i+2]=')';
+                i=i+2;                   break; //gen(  \  )  \  ()
+      default:      ;break;//gen_op,gen_expr
+    }
+    }
 }
 
 int main(int argc, char *argv[]) {
