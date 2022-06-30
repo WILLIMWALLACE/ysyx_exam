5072f499805430c513879e499204c0858377918e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 12:30:04 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:30:04 up 2 days,  3:29,  1 user,  load average: 1.64, 0.91, 0.51

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -99,24 +99,24 @@ static bool make_token(char *e) {
         switch (rules[i].token_type) {
           case TK_NOTYPE:
           {tokens[nr_token].type=rules[i].token_type;        break;}  
-          case '(':      {tokens->type=rules[i].token_type; 
-            for(j=0;j<substr_len;j++){tokens->str[j] = *(substr_start+j);}
-              tokens->str[j] = '\0';   nr_token++ ;                break;} 
-          case ')':      {tokens->type=rules[i].token_type; 
-            for(j=0;j<substr_len;j++){tokens->str[j] = *(substr_start+j);}
-              tokens->str[j] = '\0';   nr_token++ ;                break;}  
-          case '-':      {tokens->type=rules[i].token_type; 
-          for(j=0;j<substr_len;j++){tokens->str[j] = *(substr_start+j);}
-              tokens->str[j] = '\0';   nr_token++ ;                break;}  
-          case '*':      {tokens->type=rules[i].token_type; 
-           for(j=0;j<substr_len;j++){tokens->str[j] = *(substr_start+j);}
-              tokens->str[j] = '\0';   nr_token++ ;                break;}  
+          case '(':      {tokens[nr_token].type=rules[i].token_type; 
+              for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
+              tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}  
+          case ')':      {tokens[nr_token].type=rules[i].token_type; 
+              for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
+              tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}  
+          case '-':    {tokens[nr_token].type=rules[i].token_type; 
+              for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
+              tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}   
+          case '*':      {tokens[nr_token].type=rules[i].token_type; 
+              for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
+              tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}  
           case '+':      {tokens[nr_token].type=rules[i].token_type; 
           for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
               tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}  
-          case '/':      {tokens->type=rules[i].token_type; 
-           for(j=0;j<substr_len;j++){tokens->str[j] = *(substr_start+j);}
-              tokens->str[j] = '\0';   nr_token++ ;                break;}  
+          case '/':      {tokens[nr_token].type=rules[i].token_type; 
+              for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
+              tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}   
           case TK_NUMD:  {tokens[nr_token].type=rules[i].token_type; 
               for(j=0;j<substr_len;j++){tokens[nr_token].str[j] = *(substr_start+j);}
               tokens[nr_token].str[j] = '\0';   nr_token++ ;                break;}  
