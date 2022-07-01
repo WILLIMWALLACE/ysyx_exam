5b57c6e12411eff675b4d0faeff7f96783de6bd2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 21:10:29 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:10:29 up 3 days, 12:10,  1 user,  load average: 0.47, 0.53, 0.51

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -2,7 +2,6 @@
 #include <cpu/decode.h>
 #include <cpu/difftest.h>
 #include <locale.h>
-
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -10,6 +9,9 @@
  */
 #define MAX_INST_TO_PRINT 10
 
+/***********mo fang menuconfig***********/
+#define CONFIG_WATCHPOINT 1
+//////////////////////////////////////
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
@@ -23,6 +25,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+#ifdef CONFIG_WATCHPOINT  
+  
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
