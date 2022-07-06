8c305cd91025592f6a9a96dc43d899c38c6fd361
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 20:02:42 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:42 up 2 days, 21:15,  1 user,  load average: 0.38, 0.24, 0.19

--- npc/vsrc/ysyx_22040365_top.v
+++ npc/vsrc/ysyx_22040365_top.v
@@ -9,7 +9,9 @@ module ysyx_22040365_top(
 
 //////DPI-C///////////////
 import "DPI-C" function int quit(int a);
-	quit(out);
+initial begin
+	$display("%x",quit(out));
+end
 //common
 //wire[7:0]	inst_type;
 wire[1:0] 	inst_type;
