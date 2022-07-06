451e6ab2c5ecc8201db05400ae7ffa1c6b24b9ad
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 15:23:40 +0800

    >  sim RTL ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:23:40 up 2 days, 16:36,  1 user,  load average: 0.12, 0.12, 0.09

--- npc/vsrc/ysyx_22040365_regfile.v
+++ npc/vsrc/ysyx_22040365_regfile.v
@@ -7,14 +7,14 @@ module ysyx_22040365_regfile  (
   input [4:0] raddr,
   output[63:0] rdata
 );
-  reg [63:0] rf [2:0];
+  reg [63:0] regs [2:0];
   always @(posedge clk) begin
-    if(wen) rf[waddr] <= wdata;
+    if(wen) regs[waddr] <= wdata;
   end
 // always @(*) begin
 //    if(ren_rs1) rdata     = rf[raddr]; 
 // end
-assign rdata = (ren_rs1==1'b1) ? rf[raddr] : 0;
+assign rdata = (ren_rs1==1'b1) ? regs[raddr] : 0;
 
 
 endmodule
