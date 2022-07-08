158368527fc439806d43f97f32d1656421ee4b5e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 16:36:27 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:27 up  7:24,  1 user,  load average: 0.34, 0.34, 0.30

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,8 +5,78 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+//zhi.shu 
+static uint64_t my_pow(int x, int y){
+  uint64_t sum = 1;
+  while (y--)
+  {
+    sum *= x;
+  }
+  return sum;
+}
+//jin.zhi.zhuan.huan
+static char *base_convert(uint64_t num,int base){
+  static char num_lib[]="0123456789";  //ke.xuan.can.shu
+  
+  static char buf[60];                 //
+  char *str;                           //huan.cun
+  str = &buf[59];
+  *str = '\0';                          //mo.wei.tian.jia '\0',jie.shu.biao.zhi
+  do{
+    *--str = num_lib[num%base];
+    num   /= base;
+  }while(num!=0);
+  return (str);
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list exp_val;//printf yin hao hou mian shi ji zhi
+  int num_val,rnum_val; //for %d
+  char *ptr = NULL;      //for %s
+  char count,single_h;   //calculate the length of num_val,dan.ge. cong.gao.dao.di
+  va_start(exp_val,fmt); //initial exp_val->'a' ***printf("%c",'a');
+
+  while (*fmt != '\0')
+  {
+    if(*fmt == '%')
+    {
+      fmt++;
+      switch(*fmt){
+        case 'd': 
+        num_val  = va_arg(exp_val,int); //return exp_val de xia.yi.ge int lei.xing
+        rnum_val = num_val;            //huan.cun shu zhi
+        count    = 0;
+        while(rnum_val){   //calculate the length of num_val
+          count++;
+          rnum_val /= 10;
+        }
+        rnum_val = num_val; //chong.xin.huo.qu.'num'.de.shi.ji.zhi
+        while(count){
+          single_h = rnum_val/my_pow(10,count-1);//mei.ci.qu.chu.gao.wei.
+          rnum_val %= my_pow(10,count-1); //yu.shu.->sheng.xia.de.di.wei.shu.zhi
+          putch(single_h+'0');
+          count--;
+        }   break;
+        case 's':
+        ptr = va_arg(exp_val,char *);
+        putstr(ptr);  break;
+        //case 'c':
+      }   
+    }
+    else if(*fmt == '\n'){
+      putch('\n');
+    }
+    else if(*fmt == '\r'){
+      putch('\r');
+    }
+    else{
+      putch(*fmt); 
+    }
+    fmt++;
+  }
+  va_end(exp_val);
+  return 0;
+  //panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -14,7 +84,46 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list exp_val;
+  int num_val;
+  char *ptr  = NULL;
+  //char *temp = NULL;
+
+  va_start(exp_val,fmt);  //ti.qu
+  for(;*fmt != '\0'; fmt++)
+  {
+    while (*fmt != '%')
+    {
+      *out = *fmt;   
+      if(*fmt == '\0'){ break; }
+      out++;  fmt++;     //bu.shi.guan.jian.zi, zhi.jie.bao.cun.  
+    }
+    if(*fmt == '\0') {  break; }
+    fmt++;              //chu.xian.'%', xiang.hou.yi.wei, de.dao.'csd'deng.
+    switch(*fmt){
+      case 'd':
+      num_val = va_arg(exp_val,int); //bao.cun.shi.jin.zhi.shu
+      if(num_val<0){
+        num_val = -num_val;
+        *out    = '-';
+        out++;
+      }
+      ptr = base_convert(num_val,10); //int -> char'01231'
+      while(*ptr != '\0'){
+        *out = *ptr; ptr++; out++;
+      }break;
+      case 's':
+      ptr = va_arg(exp_val,char *);
+      while(*ptr != '\0'){
+        *out = *ptr; ptr++; out++;
+      }  break;
+      default:printf("need %s or %d\n"); assert(0); 
+    }
+  }
+  *out = '\0';
+  va_end(exp_val);
+  return 0;
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
