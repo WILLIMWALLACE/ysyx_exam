176fa85efca590cb5a66bece77b2de4b3d5d0848
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-09 10:56:37 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:56:37 up 1 day,  1:44,  1 user,  load average: 0.24, 0.31, 0.20

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -3,14 +3,14 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 void fifo_itrace_tran();
 int is_exit_status_bad() {
-  int flag;
+  //int flag;
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
   
-  if(!good){
-   fifo_itrace_tran();
-    flag = good;}
-  if(good) {flag = !good;}
+  if(!good)
+  { fifo_itrace_tran(); }
+    //flag = good;}
+  //if(good) {flag = !good;}
  
-  return flag;
+  return !good;
 }
