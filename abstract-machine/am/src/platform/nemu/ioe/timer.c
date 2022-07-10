6e9e4a42bcc823b937e858a20748615cbeeceb3f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-10 10:57:19 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:57:19 up 2 days,  1:44,  1 user,  load average: 0.40, 0.23, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,13 +2,14 @@
 #include <nemu.h>
 
 void __am_timer_init() {
+
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   int32_t time_h,time_l;
   time_h = inl(RTC_ADDR);
   time_l = inl(RTC_ADDR+0x4); //offset 4byte(4c-48),mei.ci.du.chu.32bit
-  uptime->us = (((int64_t) time_h << 32) + time_l);
+  uptime->us = (((int64_t) time_h << 32) + time_l) *1000000;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
