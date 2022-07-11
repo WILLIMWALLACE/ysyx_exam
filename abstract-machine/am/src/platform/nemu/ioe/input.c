f163b0c054267e05705daaf44081f72ba910a2ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 23:49:46 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:49:46 up  6:54,  1 user,  load average: 0.91, 0.58, 0.47

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,15 +4,17 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
- 
+  int32_t  flag;
   uint32_t key_input =  inl(KBD_ADDR);
-  kbd->keydown = key_input & KEYDOWN_MASK;
-    printf("key_down = %d\n",kbd->keydown);
-  if(kbd->keydown != 0){
+  flag = key_input & KEYDOWN_MASK;
+   // printf("key_down = %d\n",kbd->keydown);
+  if(flag != 0){
+    kbd->keydown = true;
     kbd->keycode = key_input & (~KEYDOWN_MASK);
     //printf("key_input = %d\n",key_input);
   }
   else {
+    kbd->keydown = true;
     kbd->keycode = AM_KEY_NONE;
   }
   //printf("key_input = %d\n",key_input);
