d242e624e39166aca2b3831b96d4a3390504f057
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 10:33:22 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:33:22 up 1 day,  1:20,  1 user,  load average: 0.15, 0.07, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -43,8 +43,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   //pixel's  zuo.biao, kuan.chang
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  printf("x=%d  ,y=%d,  w=%d,  h=%d\n",x,y,w,h);
   //store address  similar to _am_gpu_init
-  uint32_t *fb = (uint32_t *)(uintptr_t) FB_ADDR;
+  uint32_t *fb = (uint32_t *)(uintptr_t) FB_ADDR; 
+  //di.zhi.da.xiao..shi..'uintptr_r',zhi.zhen.bian.liang..shi..32.wei.
   //pixel == 0, wu.xiao
   if (w == 0 || h == 0) {return;}
   //zhi.zhen.cun.ru, xiang.su
