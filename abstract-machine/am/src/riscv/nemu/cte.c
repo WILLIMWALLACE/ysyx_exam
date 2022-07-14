9c0a62a33a4c1b2621b55c324c66545a1fba04a1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 19:57:14 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:14 up 10:47,  1 user,  load average: 0.33, 0.27, 0.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0xffffffff: printf("chufashijian\n");
+      case 0xffffffffffffffff: printf("chufashijian\n");
       ev.event = EVENT_YIELD; break; 
       default: ev.event = EVENT_ERROR; break;
     }
