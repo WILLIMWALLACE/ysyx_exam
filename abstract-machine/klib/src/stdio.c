d9403f3585f7b57180352eb317732acb338133a8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 19:13:29 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:29 up 10:04,  1 user,  load average: 0.36, 0.36, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,14 +6,14 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 //zhi.shu 
-static uint64_t my_pow(int x, int y){
+/*static uint64_t my_pow(int x, int y){
   uint64_t sum = 1;
   while (y--)
   {
     sum *= x;
   }
   return sum;
-}
+}*/
 //jin.zhi.zhuan.huan
 static char *base_convert(uint64_t num,int base){
   static char num_lib[]="0123456789";  //ke.xuan.can.shu
@@ -29,7 +29,7 @@ static char *base_convert(uint64_t num,int base){
   return (str);
 }
 
-int printf(const char *fmt, ...) {
+/*int printf(const char *fmt, ...) {
   va_list exp_val;//printf yin hao hou mian shi ji zhi
   int num_val,rnum_val; //for %d
   char *ptr = NULL;      //for %s
@@ -77,8 +77,166 @@ int printf(const char *fmt, ...) {
   va_end(exp_val);
   return 0;
   //panic("Not implemented");
+}*/
+/////////////////////////////////
+char str[11];
+char tab[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+
+char *out_num(long num,int base,char leader_m,int minwidth){
+  int i=0;
+  int len=0;
+  long num_;
+  //num_ is positive num
+  if(num<0){
+    num_ = -num;
+  }else{
+    num_ = num;
+  }
+  //num_ = 0
+  if(num_==0){
+    if(minwidth==0){
+      str[0] = '0';
+      str[1] = '\0';
+    } else{
+      str[minwidth]   = '\0';
+      str[minwidth-1] = '0' ;
+      int width = minwidth - 2;
+      for(;width>=0;width--){
+        str[width] = leader_m;
+      }
+    }
+  }
+  //num_ != 0
+  else{
+    for(i=1;num_/i!=0;i*=base)
+    len++;
+    if(len<minwidth){
+      str[minwidth]   = '\0';
+      for(;num_!=0;num_/= base){
+        minwidth -= 1;
+        str[minwidth] = tab[num_ % base];
+      }
+      for(minwidth-=1;minwidth!=0;minwidth--){
+        str[minwidth] = leader_m;
+      }
+      if(num>0) str[minwidth] = leader_m;
+      else str[minwidth] = '-';
+    }else{
+      if(num>0){
+        str[len] = '\0';
+        for(len-=1;len>=0;len--){
+          str[len] = tab[num_%base];
+          num_ /= base;
+        }
+      }else{
+        str[len+1] = '\0';
+        for(;len!=0;len--){
+          str[len] = tab[num_%base];
+          num_ /= base;
+        }
+        str[len] = '-';
+      }
+    }
+  }
+  return str;
 }
 
+
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap,fmt);
+  while(*fmt!='\0'){
+    switch(*fmt){
+      case '%':{  
+        fmt++;
+        int minwidth = 0;
+        char leader_m = ' ';
+        //leader mark
+        if(*fmt == '0'){
+          leader_m = '0';
+          fmt++;
+        }
+        //the length of data
+        while(*fmt>'0' && *fmt<'9'){
+          minwidth *= 10;
+          minwidth += *fmt -'0';
+          fmt++;
+        }
+        switch(*fmt){
+          case'c':{
+            putch (va_arg(ap,int));
+            fmt++;
+            break;
+          }
+          case's':{
+            char* str= va_arg(ap,char*);
+            while(*str!='\0' && str!=NULL){
+              putch (*str);
+              str++; 
+            }
+            fmt++;
+            break;
+          }
+          case'd':{
+            char* str_num = out_num(va_arg(ap,int),10,leader_m, minwidth);
+            while(*str_num!='\0'){
+              putch (*str_num);
+              str_num++; 
+            }
+            fmt++;
+            break;
+          }
+          case'o':{
+            char* str_num = out_num(va_arg(ap,unsigned int),8,leader_m, minwidth);
+            while(*str_num!='\0'){
+              putch (*str_num);
+              str_num++; 
+            }
+            fmt++;
+            break;
+          }
+          case'u':{
+            char* str_num = out_num(va_arg(ap,unsigned int),10,leader_m, minwidth);
+            while(*str_num!='\0'){
+              putch (*str_num);
+              str_num++; 
+            }
+            fmt++;
+            break;
+          }
+          case'x':{
+            char* str_num = out_num(va_arg(ap,unsigned int),16,leader_m, minwidth);
+            while(*str_num!='\0'){
+              putch (*str_num);
+              str_num++; 
+            }
+            fmt++;
+            break;
+          }
+          case'%':{
+            putch(*fmt);
+            fmt++;
+            break;
+          }
+        }
+        break;
+      } 
+      default:{
+      putch(*fmt);
+      fmt++;
+      break;
+      }
+    }
+    
+  }
+  va_end(ap);
+  return 0;
+}
+
+
+
+
+/////////////////////////////////////
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
