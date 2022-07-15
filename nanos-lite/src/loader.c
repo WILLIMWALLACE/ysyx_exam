bb9edeedde0757501149e4107e18ad71fa292007
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 22:39:17 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:39:17 up 13:25,  1 user,  load average: 0.41, 0.39, 0.45

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,9 +16,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
   Elf_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_phoff, ehdr.e_phentsize*ehdr.e_phnum);
+  printf("offset=%d,len=%d\n",ehdr.e_phoff,ehdr.e_phentsize*ehdr.e_phnum);
   for(int i=0; i<ehdr.e_phnum; i++){
     if(phdr[i].p_type == PT_LOAD){
-      printf("phdr[%d].offset=%s\n",i,phdr[i].p_offset);
+      //printf("phdr[%d].offset=%s\n",i,phdr[i].p_offset);
       ramdisk_read((void*)phdr[i].p_vaddr,phdr[i].p_offset,phdr[i].p_memsz);
       printf("vaddr=%d,filez=%d,memsize=%d\n",phdr[i].p_vaddr,phdr[i].p_filesz,phdr[i].p_memsz);
       memset((void*)(phdr[i].p_vaddr + phdr[i].p_filesz),0,(phdr[i].p_memsz - phdr[i].p_filesz));
