25c718ed600c9c43393b66d29ab6569b917e7d72
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 22:12:12 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:12:12 up 1 day, 12:58,  1 user,  load average: 0.49, 0.33, 0.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,5 @@
 #include <common.h>
 #include "syscall.h"
-
 enum {
   SYS_exit,
   SYS_yield,
@@ -23,7 +22,7 @@ enum {
   SYS_times,
   SYS_gettimeofday
 };
-
+////////////////  strace is immplemented here////////////////
 static int sys_yield(Context *c){
   yield();
   c->GPRx = 0;
@@ -35,23 +34,47 @@ static int sys_exit(Context *c,uint64_t a){
   printf("***********STRACE**************\nmcause=0,syscall_name=SYS_EXIT,ret_value=0\n");
   halt(a);
   c->GPRx = 0;
-  
   return 0;
 } 
 
-////////////////  strace is immplemented here////////////////
+static int sys_write(int fd,  char *buf, size_t count,Context *c){
+  if((fd!=1) || (fd!=2)){ //stdout==1(with buffer),stderr==2(no buffer)
+    printf("fd is not valid!\n");
+    c->GPRx = -1;
+    assert(0);
+  }
+  else if(count == 0){
+    c->GPRx = 0;
+  }
+  else{
+    int ret_cnt=0;
+    while(count!=0){
+      //if(*buf != '\0'){
+        putch(*buf);
+      //}
+      buf++; count--; ret_cnt++;
+    }
+  c->GPRx = ret_cnt;
+  //retrun c->GPRx;
+  }
+  printf("***********STRACE**************\nmcause=4,syscall_name=SYS_WRITE,ret_value=0\n");    
+  return c->GPRx;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//a7,mcause
-  a[1] = c->GPR2;//a0,void _exit(int status)->status
-
-
+  a[1] = c->GPR2;//a0,void _exit(int status)->status****a0=fd
+  a[2] = c->GPR3;//****a1=buf_addr****
+  a[3] = c->GPR4;//****a2=count****
+  //char *temp = NULL;
+  //temp = (char*)a[2];
   switch (a[0]) {
     case SYS_exit :sys_exit(c,a[1]);   break;
-    case SYS_yield:sys_yield(c);      break;
+    case SYS_yield:sys_yield(c);       break;
+    case SYS_write:sys_write(a[1],(char*)a[2],a[3],c);        break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
-
 
