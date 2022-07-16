0ebb55782bd4855412da3a04f311a605772361f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 19:37:45 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:37:45 up 1 day, 10:24,  1 user,  load average: 0.73, 0.26, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -27,20 +27,23 @@ enum {
 static int sys_yield(Context *c){
   yield();
   c->GPRx = 0;
+  printf("mcause=1,syscall_name=SYS_YIELD,ret_value=0\n");
   return 0;
 }
 
 static int sys_exit(Context *c,uint64_t a){
   halt(a);
   c->GPRx = 0;
+  printf("mcause=0,syscall_name=SYS_EXIT,ret_value=0\n");
   return 0;
 } 
 
-
+////////////////  strace is immplemented here////////////////
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//a7,mcause
   a[1] = c->GPR2;//a0,void _exit(int status)->status
+
 
   switch (a[0]) {
     case SYS_exit :sys_exit(c,a[1]);   break;
