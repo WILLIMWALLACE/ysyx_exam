3d947f885e6d667bf3034b4c26e181259db7b218
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 17:03:47 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:03:47 up 1 day,  7:50,  1 user,  load average: 1.00, 0.65, 0.51

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -24,24 +24,27 @@ enum {
   SYS_gettimeofday
 };
 
-static int sys_yield(){
+static int sys_yield(Context *c){
   yield();
+  c->GPRx = 0;
   return 0;
 }
 
-//static int sys_exit(){
- // halt();
-
-//} 
+static int sys_exit(Context *c,uint64_t a){
+  halt(a);
+  c->GPRx = 0;
+  return 0;
+} 
 
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//a7,mcause
+  a[1] = c->GPR2;//a0,void _exit(int status)->status
 
   switch (a[0]) {
-    //case SYS_exit :sys_exit();
-    case SYS_yield:sys_yield(); break;
+    case SYS_exit :sys_exit(c,a[1]);   break;
+    case SYS_yield:sys_yield(c);      break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
