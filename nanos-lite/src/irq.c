5cb01d4bed13ed060444cd5ad655cf2ad8b48be2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 16:42:11 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:42:11 up 1 day,  7:28,  1 user,  load average: 0.34, 0.38, 0.41

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,10 +1,11 @@
 #include <common.h>
+void do_syscall(Context *c) ;
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("识别自陷异常yield\n");break;
-    case EVENT_SYSCALL: 
-    printf("识别系统调用异常 SYS_CALL\n"); break;
+    case EVENT_SYSCALL: printf("识别系统调用异常 SYS_CALL\n");
+                        do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   return c;
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,50 @@
 #include <common.h>
 #include "syscall.h"
+
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
+static int sys_yield(){
+  yield();
+  return 0;
+}
+
+//static int sys_exit(){
+ // halt();
+
+//} 
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
+  a[0] = c->GPR1;//a7,mcause
 
   switch (a[0]) {
+    //case SYS_exit :sys_exit();
+    case SYS_yield:sys_yield();
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
+
