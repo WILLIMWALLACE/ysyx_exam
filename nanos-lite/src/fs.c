261acc527302dc1ea313962cc3b749309d638553
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 22:41:29 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:41:29 up 12:49,  1 user,  load average: 0.09, 0.08, 0.13

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -106,8 +106,8 @@ static Finfo file_table[] __attribute__((used)) = {
        }  
        printf("\ncount=%d\n",count);
       ramdisk_read(buf,file_table[fd].lseek_off, count);//sizeof(ehdr)
-      //c->GPRx = strlen(buf);
-      c->GPRx = count;
+      c->GPRx = strlen(buf);
+      file_table[fd].lseek_off += count; 
       printf("***********STRACE**************\nmcause=3,syscall_name=SYS_read_file,ret_value=%d\n",c->GPRx);
        return c->GPRx;
       //assert(0);
