217044b51f343c4bff53369ff9f884200d168d1f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 17:43:14 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:14 up 1 day,  7:50,  1 user,  load average: 0.65, 0.65, 0.56

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -99,7 +99,7 @@ off_t _lseek(int fd, off_t offset, int whence) {
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
-  uint64_t tv->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
+  uint64_t tv->tv_sec = io_read(AM_TIMER_UPTIME).us ;
   printf("tv->tv_sec=%d\n",tv->tv_sec);
     _syscall_(SYS_gettimeofday,tv->tv_sec,0,0);
   return 0;
