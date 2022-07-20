9f2c87ece12d7a647797112added35f6b9dbd0f2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 17:49:56 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:49:56 up  1:33,  1 user,  load average: 0.22, 0.38, 0.40

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -45,8 +45,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
         buf++; temp_down++;
         ret_cnt++;
       }  
-      free(temp_down);
       memset(temp_down,0,strlen(temp_down));
+      free(temp_down);
       return ret_cnt;
     }
     else if(!ev.keydown){ //up
