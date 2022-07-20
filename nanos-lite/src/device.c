df8b78d4d04d700e76fdfd6a10955bc86dfd41dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 20:04:33 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:33 up  3:48,  1 user,  load average: 0.80, 0.50, 0.30

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,7 +27,7 @@ size_t serial_write(const char *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if (ev.keycode == AM_KEY_NONE) { return 0;}
+  if (ev.keycode == AM_KEY_NONE) { memset(buf,0,strlen(buf)); return 0;}
   else{ 
     //memset(buf,0,1);
     //int ret_cnt=0;
