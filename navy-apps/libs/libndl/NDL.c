72a8306595d10e9750bfb68a888294c065421d97
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 11:42:48 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:48 up 2 days,  1:50,  1 user,  load average: 0.37, 0.31, 0.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,18 +18,18 @@ uint32_t NDL_GetTicks() {
 // 读出一条事件信息, 将其写入`buf`中, 最长写入`len`字节
 // 若读出了有效的事件, 函数返回1, 否则返回0
 int NDL_PollEvent(char *buf, int len) {
-  FILE *fp;
-  fp = fopen("/dev/events","r");
-  if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
-  else{
+  //FILE *fp;
+  //fp = fopen("/dev/events","r");
+  //if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
+  //else{
     //fread(buf,len,1,fp);
-    int fd = fileno(fp);
-    read(fd,0,0);
-    fclose(fp);
-    if(*buf==NULL){return 0;}
-    else{return 1;}
-  }
-  //return 0;
+    //int fd = fileno(fp);
+    read(3,0,0);
+    //fclose(fp);
+    //if(*buf==NULL){return 0;}
+    //else{return 1;}
+ // }
+  return 0;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
