bec12aacf5c19d98942435a661b47febffa70369
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 18:33:29 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:29 up  2:17,  1 user,  load average: 0.23, 0.34, 0.34

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -26,7 +26,7 @@ int NDL_PollEvent(char *buf, int len) {
     //fread(buf,len,1,fp);
     int fd = fileno(fp);
     *buf = NULL;
-    memset(buf,0,strlen(buf));
+    //memset(buf,0,strlen(buf));
     int flag = read(fd,buf,0);
     fclose(fp);
     if(flag == 0){return 0;}
