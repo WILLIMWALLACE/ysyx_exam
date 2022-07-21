025eea12cd0f50cc4625755c093e56518bef9372
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 23:22:11 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:22:11 up 1 day,  7:05,  1 user,  load average: 0.80, 0.41, 0.32

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -84,10 +84,10 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     //printf("&buffer[y+i]=%d,pixels=%d,w=%d\n",&buffer[y+i], pixels, w);
     //memcpy(&buffer[(y+i)*w], pixels, w*sizeof(uint32_t));
     printf("%d\n",pixels);
-    memcpy(buffer, pixels, 400*300);
+    //memcpy(buffer, pixels, 400*300);
    // printf("buffer=%d,pixels=%s\n",*buffer,pixels);
     printf("fu zhi wan cheng\n");
-    write(fd,buffer,w);
+    write(fd,pixels,w);
     printf("system call write\n");
   //}  
 }
