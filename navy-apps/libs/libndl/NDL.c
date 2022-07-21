36656b16b4ed937ef181e79f852373a67619a1b6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 14:52:43 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:52:43 up 22:36,  1 user,  load average: 0.39, 0.37, 0.20

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -52,7 +52,7 @@ void NDL_OpenCanvas(int *w, int *h) {
    // printf("zhihou %d%d\n",*w,*h);
    // fscanf(fd,  ,w);
     //printf("NDLw=%d,NDLh=%d\n",*w,*h);
-    read(fd, buf, 0);
+    //read(fd, buf, 0);
     //sscanf(buf,"%d",h);
     //printf("NDLh=%s\n",buf);
     fclose(fp);
