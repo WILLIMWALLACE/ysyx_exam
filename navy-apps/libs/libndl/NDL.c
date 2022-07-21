8d7e57d5c26237e08d719aafbb0583d2c5020deb
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 22:48:50 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:48:50 up 1 day,  6:32,  1 user,  load average: 0.84, 0.62, 0.43

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -80,13 +80,13 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
    if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
    int fd = fileno(fp);
   for(int i=0;i<h;i++,pixels+=w){
-    printf("jin ru fu zhi\n");
-    printf("&buffer[y+i]=%d,pixels=%d,w=%d\n",&buffer[y+i], pixels, w);
+    //printf("jin ru fu zhi\n");
+   // printf("&buffer[y+i]=%d,pixels=%d,w=%d\n",&buffer[y+i], pixels, w);
     memcpy(&buffer[y+i], pixels, w);
-    printf("buffer=%d,pixels=%s\n",*buffer,pixels);
-    printf("fu zhi wan cheng\n");
+   // printf("buffer=%d,pixels=%s\n",*buffer,pixels);
+   // printf("fu zhi wan cheng\n");
     write(fd,buffer,w);
-    printf("system call write\n");
+   // printf("system call write\n");
   }  
 }
 
