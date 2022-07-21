8699c5944510afecffe7dc8c1232dabde18eab45
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 22:43:12 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:43:12 up 1 day,  6:26,  1 user,  load average: 0.58, 0.47, 0.34

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -74,7 +74,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int total_w = io_read(AM_GPU_CONFIG).width ;
   int x = offset%total_w;
   int y = offset/total_w;
-  io_write(AM_GPU_FBDRAW, x , y , (char*)buf, total_w, 1, false);
+  io_write(AM_GPU_FBDRAW, x , y , (char*)buf, total_w, 0, false);
   return 0;
 }
 
