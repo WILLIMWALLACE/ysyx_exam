e8649ba252cf38a37823a89b45ac9a9094df35d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 10:20:07 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:07 up 1 day, 18:03,  1 user,  load average: 1.10, 0.56, 0.45

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -80,21 +80,21 @@ void NDL_OpenCanvas(int *w, int *h) {
 // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
 // 像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  uint32_t *buffer[w*4];//every address present one byte
+  uint32_t *buffer[w*32];//every address present one byte
    FILE *fp;
    fp = fopen("/dev/fb","r");
    if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
    int fd = fileno(fp);
-   memset(buffer,0,w*4);
+   memset(buffer,0,w*32);
   for(int i=0;i<h;i++,pixels+=w){
     //printf("jin ru fu zhi\n");
     //printf("&buffer[y+i]=%d,pixels=%d,w=%d\n",&buffer[y+i], pixels, w);
     //memcpy(&buffer[(y+i)*w], pixels, w*4);
     //printf("%d\n",pixels);
-    memcpy(buffer, pixels, w*4);
+    memcpy(buffer, pixels, w*32);
     // printf("buffer=%d,pixels=%s\n",*buffer,pixels);
     //printf("fu zhi wan cheng\n");
-    write(fd,buffer,w*4);
+    write(fd,buffer,w);
     printf("w****=%d\n",w);
     //printf("system call write\n");
   }  
