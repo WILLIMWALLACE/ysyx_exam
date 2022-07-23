a18d1134405afb03588ad0e225fcf1141d27b03d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 15:55:33 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:55:33 up 1 day,  1:10,  1 user,  load average: 0.76, 0.71, 0.50

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -23,8 +23,8 @@ int SDL_WaitEvent(SDL_Event *event) {
   char key_name[40]; //there is 82 names; 
   
   NDL_PollEvent(key_buf, sizeof(key_buf));//obtain key infomation
-  //memset(NDL_key_type,0,20); 
-  //memset(key_name,0,40);
+  memset(NDL_key_type,0,20); 
+  memset(key_name,0,40);
   sscanf(key_buf,"%s %s",NDL_key_type,key_name);
    //printf("NDL_key_type == %s,   key_name==%s\n",NDL_key_type,key_name);
   if(strcmp(NDL_key_type,"kd")==0){
@@ -35,7 +35,7 @@ int SDL_WaitEvent(SDL_Event *event) {
       {event->key.keysym.sym = i;printf("receive key_id=%d\n",i);break;}
     }
   }
-  if(NDL_key_type == "ku"){
+  if(strcmp(NDL_key_type,"ku")==0){
     event->type           = SDL_KEYUP;
     for(int i=0;i<(sizeof(key_name)/sizeof(char));i++){
       if(strcmp(key_name,keyname[i])==0)
