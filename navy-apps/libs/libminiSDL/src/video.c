bb047fbe980357aead158a0fcf85d549b6e8fbea
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 11:16:09 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:09 up 20:30,  1 user,  load average: 0.49, 0.25, 0.29

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -142,6 +142,7 @@ void SDL_SetPalette(SDL_Surface *s, int flags, SDL_Color *colors, int firstcolor
       uint8_t g = colors[i].g;
       uint8_t b = colors[i].b;
     }
+    printf("s_w=%d,s_h=%d\n",s->w,s->h);
     SDL_UpdateRect(s, 0, 0, s->w, s->h);
   }
 }
