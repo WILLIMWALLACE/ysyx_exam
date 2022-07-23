a48b00cec4b68d7e422cee0da69131a12903a511
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 22:43:33 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:43:33 up 1 day,  7:58,  1 user,  load average: 0.75, 0.40, 0.25

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -88,29 +88,6 @@ static Finfo file_table[] __attribute__((used)) = {
   return c->GPRx;
 }
 
- ///////////////  fs_lseek  ///////////////////
-long sys_lseek(int fd, long offset, int whence){
-    switch(whence){
-      case SEEK_SET: file_table[fd].lseek_off = offset;
-      //printf("seek_set\n"); 
-      break;
-      case SEEK_CUR: file_table[fd].lseek_off += offset;
-      //printf("seek_cur\n");
-      break;
-      case SEEK_END: file_table[fd].lseek_off = file_table[fd].size+offset;
-      //printf("seek_end\n");
-      break;
-      default : printf("invalid whence!\n");  assert(0);
-    }
-    if(file_table[fd].lseek_off<1||file_table[fd].lseek_off>file_table[fd].size){
-      return -1;
-    }
-    else{
-      //printf("lseek_off = %d\n",file_table[fd].lseek_off);
-      return file_table[fd].lseek_off;
-    }
-}
-
 /////////////// fs_read  /////////////////////
  //int sys_read(int fd,void *buf,size_t count,Context *c){
    int sys_read(int fd,void *buf,size_t count){
@@ -133,6 +110,28 @@ ramdisk_read(buf,file_table[fd].lseek_off+file_table[fd].disk_offset,count);//si
     }
 }
 
+ ///////////////  fs_lseek  ///////////////////
+long sys_lseek(int fd, long offset, int whence){
+    switch(whence){
+      case SEEK_SET: file_table[fd].lseek_off = offset;
+      //printf("seek_set\n"); 
+      break;
+      case SEEK_CUR: file_table[fd].lseek_off += offset;
+      //printf("seek_cur\n");
+      break;
+      case SEEK_END: file_table[fd].lseek_off = file_table[fd].size+offset;
+      //printf("seek_end\n");
+      break;
+      default : printf("invalid whence!\n");  assert(0);
+    }
+    if(file_table[fd].lseek_off<1||file_table[fd].lseek_off>file_table[fd].size){
+      return -1;
+    }
+    else{
+      //printf("lseek_off = %d\n",file_table[fd].lseek_off);
+      return file_table[fd].lseek_off;
+    }
+}
 
 ////////////////  fs_close  ////////////////////////
  int sys_close(){
