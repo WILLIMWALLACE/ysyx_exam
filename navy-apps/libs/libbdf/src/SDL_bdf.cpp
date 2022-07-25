54c11f3cd5832be60852d7cd2af6a665afc2d034
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 11:20:10 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:20:09 up 1 day,  2:00,  1 user,  load average: 0.33, 0.43, 0.33

--- navy-apps/libs/libbdf/src/SDL_bdf.cpp
+++ navy-apps/libs/libbdf/src/SDL_bdf.cpp
@@ -7,7 +7,9 @@ SDL_Surface* BDF_CreateSurface(BDF_Font *font, char ch, uint32_t fg, uint32_t bg
   if (!bm) return NULL;
   int w = font->w, h = font->h;
   uint32_t *pixels = (uint32_t *)malloc(w * h * sizeof(uint32_t));
+  printf("pixels=%s\n",pixels);
   assert(pixels);
+  printf("failed!\n");
   for (int j = 0; j < h; j ++) {
     for (int i = 0; i < w; i ++) {
       pixels[j * w + i] = ((bm[j] >> i) & 1) ? fg : bg;
