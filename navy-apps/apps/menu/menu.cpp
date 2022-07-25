6a62fe055279f0e0fdf895eebbc652aff8e403c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 10:31:00 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:31:00 up 1 day,  1:11,  1 user,  load average: 0.36, 0.27, 0.20

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -70,7 +70,7 @@ static void clear_display(void) {
 int main(int argc, char *argv[], char *envp[]) {
   SDL_Init(0);
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
-  printf("screen:x=0,y=0,w=%d,h=%d\n",screen->w,screen->h);
+  //printf("screen:x=0,y=0,w=%d,h=%d\n",screen->w,screen->h);
 
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
@@ -78,7 +78,7 @@ int main(int argc, char *argv[], char *envp[]) {
   set_i_max();
   //printf("wancheng set\n");
   while (1) {
-    printf("jinru xunhuan\n");
+    //printf("jinru xunhuan\n");
     display_menu(i_max);
     printf("finish display_menu\n");
     SDL_Event e;
@@ -144,6 +144,8 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
+  printf("screen->w=%d,logf->w=%d\n",screen->w,logo_sf->w);
+  printf("menu_main screen:w=%d,h=%d\n",rect.x,rect.y);
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
   char buf[80];
