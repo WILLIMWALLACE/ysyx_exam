0b760c16371c89ce47db7188855a6e6e8913a1ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 21:53:19 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:53:19 up 1 day, 12:33,  1 user,  load average: 0.34, 0.31, 0.27

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -5347,30 +5347,36 @@ static int stbi__info_main(stbi__context *s, int *x, int *y, int *comp)
    printf("x=%d,y=%d,comp=%d\n",*x,*y,*comp);
    #ifndef STBI_NO_JPEG
    if (stbi__jpeg_info(s, x, y, comp)) return 1;
+   printf("stbi__jpeg_info\n");
    #endif
 
    #ifndef STBI_NO_PNG
    if (stbi__png_info(s, x, y, comp))  return 1;
+    printf("stbi__png_info\n");
    #endif
 
    #ifndef STBI_NO_GIF
    if (stbi__gif_info(s, x, y, comp))  return 1;
+    printf("stbi__gif_info\n");
    #endif
 
    #ifndef STBI_NO_BMP
    if (stbi__bmp_info(s, x, y, comp))  return 1;
+     printf("stbi__bmp_info\n");
    #endif
 
 
 
    #ifndef STBI_NO_PNM
    if (stbi__pnm_info(s, x, y, comp))  return 1;
+     printf("stbi__pnm_info\n");
    #endif
 
 
    // test tga last because it's a crappy test!
    #ifndef STBI_NO_TGA
    if (stbi__tga_info(s, x, y, comp))
+   printf("stbi__tga_info\n");
        return 1;
    #endif
    return stbi__err("unknown image type", "Image not of any known type, or corrupt");
