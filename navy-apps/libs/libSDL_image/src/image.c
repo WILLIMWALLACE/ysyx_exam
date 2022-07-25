126ad81d36552f83306f1f8f1c57755d61e8816f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 20:59:57 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:57 up 1 day, 11:40,  1 user,  load average: 0.28, 0.20, 0.27

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -21,7 +21,7 @@ SDL_Surface* IMG_Load(const char *filename) {
   //obtain the num(char) of string ,from start to presetn fp
   int len = ftell(fp);
   printf("file len = %d\n",len);
-  uint8_t *pixels = malloc(len*sizeof(uint8_t));  
+  uint8_t *pixels = malloc(len);  
   //assert(pixels!=NULL);
   printf("finish malloc\n");
   fseek(fp,0,SEEK_SET);
