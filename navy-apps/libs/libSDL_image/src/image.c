b44bb424aaa9fa24bc0f97a6ff049810395ff04d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 20:44:20 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:44:20 up 1 day, 11:24,  1 user,  load average: 0.35, 0.44, 0.43

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -10,24 +10,8 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(freesrc == 0);
   return NULL;
 }
-SDL_Surface* IMG_Load(const char *filename) {
-  printf("IMG_Load:%s\n",filename);
-  FILE *fp = fopen(filename, "r+");
-  assert(fp!=NULL);
 
-  fseek(fp,0,SEEK_END);
-  int size=ftell(fp);
-  printf("SDL_Surface* IMG_Load:%d\n",size);
-  uint8_t *buf=(uint8_t *)malloc(size);
-  fseek(fp,0,SEEK_SET);
-  assert(fread(buf,size,1,fp)==1);
-  SDL_Surface* screen=STBIMG_LoadFromMemory(buf, size);
-  assert(screen!=NULL);
-  free(buf);
-  printf("=====================SDL_Surface* IMG_Load\n");
-  return screen;
-}
-/*SDL_Surface* IMG_Load(const char *filename) {
+SDL_Surface* IMG_Load(const char *filename) {
   FILE *fp = fopen(filename, "r");
   if(fp==NULL){ printf("fp==NULL,invalid filename!\n"); assert(0);}
   //fp will move to the end of file
@@ -41,7 +25,7 @@ SDL_Surface* IMG_Load(const char *filename) {
   fseek(fp,0,SEEK_SET);
 
   assert(1==fread(pixels,len,1,fp));
-  printf("pixel=%s\n",pixels);
+  printf("pixel=%d\n",pixels);
   SDL_Surface *surface = STBIMG_LoadFromMemory(pixels,len);
   printf("finish STBIMG_LoadFromMemory\n");
   if(surface==NULL) 
@@ -51,7 +35,7 @@ SDL_Surface* IMG_Load(const char *filename) {
   fclose(fp);
   
   return surface;
-}*/
+}
 
 int IMG_isPNG(SDL_RWops *src) {
   return 0;
