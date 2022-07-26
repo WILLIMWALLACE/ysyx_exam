6af37ef51498011736c8349d64ac6f9ae24bd078
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 21:50:24 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:50:24 up 10:14,  1 user,  load average: 0.60, 0.54, 0.49

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -57,20 +57,32 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   //printf("jinru fill\n");
   SDL_Rect temp_rect;
   if(dstrect==NULL){
+   
     uint32_t rect_size = (dst->h)*(dst->w);
     for(int i=0;i<rect_size;i++){
     *((uint32_t *)dst->pixels+i) = color;
     //*((uint32_t *)dst->pixels) = color;
     //(uint32_t *)dst->pixels  += i;
     }
-    NDL_DrawRect((uint32_t *)dst->pixels,0,0,dst->w,dst->h);
+     if(dst->format->palette==NULL){
+      NDL_DrawRect((uint32_t *)dst->pixels,0,0,dst->w,dst->h);
+     }
+     else{
+      NDL_DrawRect((uint8_t *)dst->pixels,0,0,dst->w,dst->h);
+     }
   }
   else{
     uint32_t rect_size = (dstrect->h)*(dstrect->w);
     for(int i=0;i<rect_size;i++){
     *((uint32_t *)dst->pixels+i) = color;
     }
- NDL_DrawRect((uint32_t *)dst->pixels,dstrect->x,dstrect->y,dstrect->w,dstrect->h);
+    if(dst->format->palette==NULL){
+      NDL_DrawRect((uint32_t *)dst->pixels,dstrect->x,dstrect->y,dstrect->w,dstrect->h);
+     }
+     else{
+      NDL_DrawRect((uint8_t *)dst->pixels,dstrect->x,dstrect->y,dstrect->w,dstrect->h);
+     }
+ 
   }
 }
 
