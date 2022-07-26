dc478b0ec7cd0e1a70691c36fd69c3d500598b97
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 16:36:53 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:53 up  5:00,  1 user,  load average: 0.94, 0.77, 0.54

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -47,16 +47,18 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 //快速以指定颜色填充矩形区域
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   //printf("jinru fill\n");
-  //SDL_Rect temp_rect;
+  SDL_Rect temp_rect;
   if(dstrect==NULL){
      //printf("fu zhi cheng gong\n");    
-    dstrect->w = dst->w;
-    dstrect->h = dst->h;
-    dstrect->x = 0;
-    dstrect->y = 0;    
+    temp_rect.w = dst->w;
+    temp_rect.h = dst->h;
+    temp_rect.x = 0;
+    temp_rect.y = 0;
+    dstrect      = &temp_rect;   
+    //printf("fu zhi cheng gong\n");    
   }//printf("dst: x=0,y=0,w=%d,h=%d\n",dst->w,dst->h);
   //printf("temp_rect: x=%d,y=%d,w=%d,h=%d\n",temp_rect.x,temp_rect.y,temp_rect.w,temp_rect.h);
-  //printf("dstrect: x=%d,y=%d,w=%d,h=%d\n",dstrect->x,dstrect->y,dstrect->w,dstrect->h);
+  printf("dstrect: x=%d,y=%d,w=%d,h=%d\n",dstrect->x,dstrect->y,dstrect->w,dstrect->h);
   uint32_t rect_size = (dstrect->h)*(dstrect->w);
   for(int i=0;i<rect_size;i++){
     *((uint32_t *)dst->pixels+i) = color;
