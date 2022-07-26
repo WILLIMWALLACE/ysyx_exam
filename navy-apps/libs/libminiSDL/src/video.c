6cd2ae799f1180a3249a9c89e72731262f991c7d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 16:48:32 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:48:32 up  5:12,  1 user,  load average: 0.40, 0.52, 0.54

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -42,10 +42,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   //printf("dst_rect:w=%d,h=%d\n",dstrect->w,dstrect->h);
   for(int i=0;i<srcrect->h;i++){
     for(int j=0;j<srcrect->w;j++){
-      //*((uint32_t *)dst->pixels+(i*dst->w+j + screen_offset_dst)) =  *((uint32_t *)src->pixels+(i*src->w+j + screen_offset_src));
-      *((uint32_t *)dst->pixels) =   *((uint32_t *)src->pixels);
-      (uint32_t *)dst->pixels += (i*dst->w+j + screen_offset_dst);
-      (uint32_t *)src->pixels += (i*src->w+j + screen_offset_src);
+      *((uint32_t *)dst->pixels+(i*dst->w+j + screen_offset_dst)) =  *((uint32_t *)src->pixels+(i*src->w+j + screen_offset_src));
+      //*((uint32_t *)dst->pixels) =   *((uint32_t *)src->pixels);
+      //(uint32_t *)dst->pixels += (i*dst->w+j + screen_offset_dst);
+      //(uint32_t *)src->pixels += (i*src->w+j + screen_offset_src);
     }
   }
 }
