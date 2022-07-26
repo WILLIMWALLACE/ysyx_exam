9823859447ba9558e317a407f6b1a391fe327e5b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 10:18:39 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:18:39 up  1:01,  1 user,  load average: 0.73, 0.39, 0.34

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -3930,7 +3930,6 @@ static int stbi__png_info_raw(stbi__png *p, int *x, int *y, int *comp)
 {
    if (!stbi__parse_png_file(p, STBI__SCAN_header, 0)) {
       stbi__rewind( p->s );
-      printf("!stbi__parse_png_file\n");
       return 0;
    }
    if (x) *x = p->s->img_x;
@@ -3943,6 +3942,7 @@ static int stbi__png_info(stbi__context *s, int *x, int *y, int *comp)
 {
    stbi__png p;
    p.s = s;
+   printf("stbi__png_info\n");
    return stbi__png_info_raw(&p, x, y, comp);
 }
 
