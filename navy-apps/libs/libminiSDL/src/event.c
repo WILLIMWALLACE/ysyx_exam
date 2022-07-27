7a7f282717097d15581fd3dac093b1a0d35cbe67
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 12:12:40 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:12:40 up 1 day, 36 min,  1 user,  load average: 0.25, 0.40, 0.48

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -24,10 +24,10 @@ int SDL_PollEvent(SDL_Event *ev) {
   char key_name[40]; //there is 82 names; 
   
   memset(key_buf,0,60);
-  printf("finish memset\n");
+  //printf("finish memset\n");
   NDL_PollEvent(key_buf, sizeof(key_buf));//obtain key infomation
-  printf("finish NDL_PollEvent\n");
-  printf("key_buf = %s\n",key_buf);
+  //printf("finish NDL_PollEvent\n");
+  //printf("key_buf = %s\n",key_buf);
   if(*key_buf == 0){ return 0;} 
   else{
   //printf("enter identify key!\n");
@@ -58,7 +58,7 @@ int SDL_PollEvent(SDL_Event *ev) {
     printf("can not identify user_event\n");
     assert(0);
    }
-   printf("exit SDL_PollEvent!\n");
+   //printf("exit SDL_PollEvent!\n");
   return 1;
   }
 }
