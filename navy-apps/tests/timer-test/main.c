08e65326a280a2facf2238a5f105eece2e799b5a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 11:21:54 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:21:54 up 23:45,  1 user,  load average: 1.05, 0.57, 0.32

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -13,10 +13,10 @@ int main() {
    while(1){
 //	    printf("jinru xun huan\n");
     //	gettimeofday(&time,NULL);
-	ms = NDL_GetTicks() - start_ms;
+	ms = NDL_GetTicks();
 //	printf("user_time=%d\n",time.tv_sec);
 	//cnt++;
-  printf("ms=%d\n",ms);
+  //printf("ms=%d\n",ms);
 	if(ms%500==0){
 	  //flag = cnt;
   	  printf("The time of day is %ld s!\n",ms);
