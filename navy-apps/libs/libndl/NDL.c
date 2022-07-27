a118bb24591299c14fbe0fea08757a009b69f363
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 19:11:15 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:11:15 up 1 day,  7:35,  1 user,  load average: 0.48, 0.61, 0.39

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -8,6 +8,7 @@
 
 static int evtdev = -1;
 static int fbdev = -1;
+static int prdisp = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
@@ -19,38 +20,39 @@ uint32_t NDL_GetTicks() {
 // 读出一条事件信息, 将其写入`buf`中, 最长写入`len`字节
 // 若读出了有效的事件, 函数返回1, 否则返回0
 int NDL_PollEvent(char *buf, int len) {
+  /*
   FILE *fp;
   fp = fopen("/dev/events","r");
-  if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
-  else{
-    //fread(buf,len,1,fp);
-    int fd = fileno(fp);
+  */
+  //if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
+  //else{
+    //int fd = fileno(fp);
     //*buf = NULL;
     memset(buf,0,strlen(buf));
     //printf("finish open\n");
-    int flag = read(fd,buf,0);
+    int flag = read(evtdev,buf,0);
     //printf("finish read\n");
     //if(buf!=NULL){ printf("receive key = %s\n",buf);}
-    fclose(fp);
+    //fclose(fp);
     if(flag == 0){return 0;}
     else{return 1;}
-  }
+//  }
   //return 0;
 }
 // 打开一张(*w) X (*h)的画布
 // 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
   ///////////////// add /////////////////////
-    FILE *fp;
+    /*FILE *fp;
     fp = fopen("/proc/dispinfo","r");
-    if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
-    else{
-    int fd = fileno(fp);
+    if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} */
+   // else{
+    //int fd = fileno(fp);
     char buffer[60]; 
     char key_info[60];
     int read_w,read_h;
     memset(buffer,0,60); memset(key_info,0,60);
-    read(fd, buffer, 0);
+    read(prdisp, buffer, 0);
     sscanf(buffer,"%s %d %s %d",key_info,&read_w,key_info,&read_h);
    // printf("key_info = %s,read_w=%d\n",key_info,read_w);
    
@@ -59,8 +61,8 @@ void NDL_OpenCanvas(int *w, int *h) {
     if(*w>=read_w || *w==0) { screen_w = read_w; *w=read_w;}
     if(*h>=read_h || *h==0) { screen_h = read_h; *h=read_h;}
 
-    fclose(fp);
-    }
+    //fclose(fp);
+    //}
     //////////////////  add  ////////////////////
   if (getenv("NWM_APP")) {
     int fbctl = 4;
@@ -85,10 +87,10 @@ void NDL_OpenCanvas(int *w, int *h) {
 // 像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   uint32_t *buffer[w*4];//every address present one byte
-   FILE *fp;
+   /*FILE *fp;
    fp = fopen("/dev/fb","r");
    if(fp == NULL){printf("bu cun zai wen jian\n");assert(0);} 
-   int fd = fileno(fp);
+   int fd = fileno(fp);*/
    memset(buffer,0,w*4);
     lseek(5,0,SEEK_SET);
   for(int i=0;i<h;i++,pixels+=w){
@@ -100,11 +102,11 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     memcpy(buffer, pixels, w*4);
     //printf("buffer=%d,pixels=%s\n",*buffer,pixels);
     //printf("fu zhi wan cheng\n");
-    write(fd,buffer,w);
+    write(fbdev,buffer,w);
     //printf("w****=%d\n",w);
     //printf("system call write\n");
   }  
-   fclose(fp);
+  //fclose(fbdev);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -122,6 +124,10 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
+  evtdev = open("/dev/events",0,0);
+  prdisp = open("/proc/dispinfo",0,0);
+  fbdev  = open("/dev/fb",0,0);
+
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
@@ -129,4 +135,7 @@ int NDL_Init(uint32_t flags) {
 }
 
 void NDL_Quit() {
+  close(evtdev);
+  close(prdisp);
+  close(fbdev);
 }
