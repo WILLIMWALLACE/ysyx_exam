b152f40d89f775ac7c87e42d8a698491629b93ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 10:56:19 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:56:19 up 23:20,  1 user,  load average: 0.61, 0.39, 0.26

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -99,7 +99,7 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  //printf("not immplement SDL_GetKeyState\n");
+  printf("enter SDL_GetKeyState\n");
   //assert(0);
 
   return keystate;
