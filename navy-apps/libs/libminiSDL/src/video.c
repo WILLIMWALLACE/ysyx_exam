05976376f1fac08bc6291f741d31a306f1f21161
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 12:15:29 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:15:29 up 1 day, 39 min,  1 user,  load average: 0.27, 0.30, 0.42

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -116,7 +116,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
    //printf("exit the for\n");
     //printf("buffer=%s\n",buffer);
     NDL_DrawRect(buffer,x,y,real_w,real_h);
-     //printf("exit tht NDL_draw\n");
+     printf("exit tht NDL_draw\n");
    }
     //lseek(5,0,SEEK_SET);
     //printf("finish update\n");printf("w=%d,h=%d\n",w,h);
