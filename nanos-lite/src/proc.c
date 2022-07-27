b88fedf1fc8b36fea72c0938a803c253175b7ce2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 16:40:58 +0800

    >  compile NEMU ysyx_22040365 姜牟旬 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:40:58 up 1 day,  5:04,  1 user,  load average: 0.73, 0.41, 0.34

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  naive_uload(NULL,"/bin/pal");
+  naive_uload(NULL,"/bin/bird");
   //printf("************finish initial processes\n");
   // load program here
 
